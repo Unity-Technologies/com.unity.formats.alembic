4f8df81cdd4b6cdd226367e45101e0e956efa8af
gatgui <gatgui@gmail.com>
2015-06-10 15:07:15 +0900

    added archive start/end time query functions more playback controls reset time to 0 when starting playback

--- AlembicImporter/Assets/AlembicImporter/Scripts/AlembicStream.cs
+++ AlembicImporter/Assets/AlembicImporter/Scripts/AlembicStream.cs
@@ -11,13 +11,21 @@ using UnityEditor;
 [ExecuteInEditMode]
 public class AlembicStream : MonoBehaviour
 {
+    public enum CycleType { Hold, Loop, Reverse, Bounce };
+
     public string m_path_to_abc;
     public float m_time;
-    public float m_timescale = 1.0f;
+    public float m_start_time;
+    public float m_end_time;
+    public float m_time_offset;
+    public float m_time_scale = 1.0f;
+    public bool m_preserve_start_time = true;
+    public CycleType m_cycle = CycleType.Hold;
     public bool m_reverse_x;
     public bool m_reverse_faces;
     bool m_loaded;
     float m_time_prev;
+    float m_time_eps = 0.001f;
     AlembicImporter.aiContext m_abc;
 
 
@@ -35,6 +43,73 @@ public class AlembicStream : MonoBehaviour
         AlembicImporter.aiDestroyContext(m_abc);
     }
 
+    float AdjustTime(float in_time)
+    {
+        float extra_offset = 0.0f;
+
+        // compute extra time offset to counter-balance effect of m_time_scale on m_start_time
+        if (m_preserve_start_time)
+        {
+            extra_offset = m_start_time * (m_time_scale - 1.0f);
+        }
+
+        float play_time = m_end_time - m_start_time;
+
+        // apply speed and offset
+        float out_time = m_time_scale * (in_time - m_time_offset) - extra_offset;
+
+        if (m_cycle == CycleType.Hold)
+        {
+            if (out_time < (m_start_time - m_time_eps))
+            {
+                out_time = m_start_time;
+            }
+            else if (out_time > (m_end_time + m_time_eps))
+            {
+                out_time = m_end_time;
+            }
+        }
+        else
+        {
+            float normalized_time = (out_time - m_start_time) / play_time;
+            float play_repeat = (float)Math.Floor(normalized_time);
+            float fraction = Math.Abs(normalized_time - play_repeat);
+
+            if (out_time < (m_start_time - m_time_eps) || out_time > (m_end_time + m_time_eps))
+            {
+                // outside original alembic range
+                if (m_cycle == CycleType.Reverse)
+                {
+                    out_time = (out_time < m_start_time ? m_end_time : m_start_time);
+                }
+                else if (m_cycle == CycleType.Loop || ((int)play_repeat % 2) == 0)
+                {
+                    out_time = m_start_time + fraction * play_time;
+                }
+                else
+                {
+                    out_time = m_end_time - fraction * play_time;
+                }
+            }
+            else
+            {
+                // inside original alembic range
+                if (m_cycle == CycleType.Reverse)
+                {
+                    out_time = m_end_time - fraction * play_time;
+                }
+            }
+        }
+
+        return out_time;
+    }
+
+    void Start()
+    {
+        m_time_prev = 0.0f;
+        m_time = 0.0f;
+    }
+
     void Update()
     {
         if (!m_loaded)
@@ -43,11 +118,12 @@ public class AlembicStream : MonoBehaviour
         }
         if(m_loaded)
         {
-            m_time += Time.deltaTime * m_timescale;
-            if (m_time_prev != m_time)
+            m_time += Time.deltaTime;
+
+            if (Math.Abs(m_time - m_time_prev) > m_time_eps)
             {
+                AlembicImporter.UpdateAbcTree(m_abc, GetComponent<Transform>(), m_reverse_x, m_reverse_faces, AdjustTime(m_time));
                 m_time_prev = m_time;
-                AlembicImporter.UpdateAbcTree(m_abc, GetComponent<Transform>(), m_reverse_x, m_reverse_faces, m_time);
             }
         }
     }
--- AlembicImporterPlugin/aiContext.cpp
+++ AlembicImporterPlugin/aiContext.cpp
@@ -3,7 +3,7 @@
 #include "aiObject.h"
 #include "aiGeometry.h"
 #include "aiContext.h"
-
+#include <limits>
 
 
 aiContext* aiContext::create()
@@ -21,6 +21,9 @@ aiContext::aiContext()
 #ifdef aiDebug
     m_magic = aiMagicCtx;
 #endif // aiDebug
+
+    m_time_range[0] = 0;
+    m_time_range[1] = 0;
 }
 
 aiContext::~aiContext()
@@ -37,8 +40,8 @@ void aiContext::gatherNodesRecursive(aiObject *n)
 {
     m_nodes.push_back(n);
     abcObject &abc = n->getAbcObject();
-    int num_children = abc.getNumChildren();
-    for (int i = 0; i < num_children; ++i) {
+    size_t num_children = abc.getNumChildren();
+    for (size_t i = 0; i < num_children; ++i) {
         abcObject abcChild = abc.getChild(i);
         aiObject *child = new aiObject(this, abcChild);
         n->addChild(child);
@@ -51,16 +54,16 @@ bool aiContext::load(const char *path)
     if (path == nullptr) return false;
 
     try {
-        aiDebugLog("trying to open AbcCoreHDF5::ReadArchive...\n");
-        m_archive = abcArchivePtr(new Abc::IArchive(AbcCoreHDF5::ReadArchive(), path));
+        aiDebugLog("trying to open AbcCoreOgawa::ReadArchive...\n");
+        m_archive = abcArchivePtr(new Abc::IArchive(AbcCoreOgawa::ReadArchive(), path));
     }
     catch (Alembic::Util::Exception e)
     {
         aiDebugLog("exception: %s\n", e.what());
 
         try {
-            aiDebugLog("trying to open AbcCoreOgawa::ReadArchive...\n");
-            m_archive = abcArchivePtr(new Abc::IArchive(AbcCoreOgawa::ReadArchive(), path));
+            aiDebugLog("trying to open AbcCoreHDF5::ReadArchive...\n");
+            m_archive = abcArchivePtr(new Abc::IArchive(AbcCoreHDF5::ReadArchive(), path));
         }
         catch (Alembic::Util::Exception e)
         {
@@ -73,13 +76,58 @@ bool aiContext::load(const char *path)
         aiObject *top = new aiObject(this, abcTop);
         gatherNodesRecursive(top);
 
+        m_time_range[0] = std::numeric_limits<double>::max();
+        m_time_range[1] = -std::numeric_limits<double>::max();
+
+        for (unsigned int i=0; i<m_archive->getNumTimeSamplings(); ++i)
+        {
+            AbcCoreAbstract::TimeSamplingPtr ts = m_archive->getTimeSampling(i);
+
+            AbcCoreAbstract::TimeSamplingType tst = ts->getTimeSamplingType();
+
+            // Note: alembic guaranties we have at least one stored time
+
+            if (tst.isCyclic() || tst.isUniform())
+            {
+                size_t num_cycles = (m_archive->getMaxNumSamplesForTimeSamplingIndex(i) / tst.getNumSamplesPerCycle());
+
+                m_time_range[0] = ts->getStoredTimes()[0];
+                m_time_range[1] = m_time_range[0] + (num_cycles - 1) * tst.getTimePerCycle();
+            }
+            else if (tst.isAcyclic())
+            {
+                m_time_range[0] = ts->getSampleTime(0);
+                m_time_range[1] = ts->getSampleTime(ts->getNumStoredTimes() - 1);
+            }
+        }
+
+        if (m_time_range[0] > m_time_range[1])
+        {
+            m_time_range[0] = 0.0;
+            m_time_range[1] = 0.0;
+        }
+
         aiDebugLog("succeeded\n");
         return true;
     }
     else {
         m_archive.reset();
+
+        m_time_range[0] = 0.0;
+        m_time_range[1] = 0.0;
+
         return false;
     }
+}
+
+float aiContext::getStartTime() const
+{
+    return float(m_time_range[0]);
+}
+
+float aiContext::getEndTime() const
+{
+    return float(m_time_range[1]);
 }
 
 aiObject* aiContext::getTopObject()
