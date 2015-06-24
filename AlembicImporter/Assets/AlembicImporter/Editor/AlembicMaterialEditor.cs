8c78c10c68fc147ec8d0eca71a3f8757d2537097
gatgui <gatgui@gmail.com>
2015-06-24 14:04:45 +0900

    display Assets folder relative paths in material editor moved material Import/Export methods from AlembicMaterialEditor to AlembicMaterial component

--- AlembicImporter/Assets/AlembicImporter/Editor/AlembicMaterialEditor.cs
+++ AlembicImporter/Assets/AlembicImporter/Editor/AlembicMaterialEditor.cs
@@ -1,8 +1,5 @@
 using System;
 using System.IO;
-using System.Xml;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
@@ -11,10 +8,6 @@ class AlembicMaterialEditor : EditorWindow
     string xmlPath = "";
     string materialFolder = "";
     GameObject assetRoot = null;
-
-    static char[] PathSep = new char[1] { '/' };
-    static char[] FaceSep = new char[1] { ',' };
-    static char[] RangeSep = new char[1] { '-' };
 
     [MenuItem ("Assets/Import Alembic Materials")]
     public static void ShowWindow()
@@ -32,6 +25,19 @@ class AlembicMaterialEditor : EditorWindow
         r.height -= vpad;
 
         return r;
+    }
+
+    string MakeDataRelativePath(string path)
+    {
+        if (path.Length > 0)
+        {
+            Uri base_uri = new Uri(Application.dataPath + "/");
+            return base_uri.MakeRelativeUri(new Uri(path)).ToString();
+        }
+        else
+        {
+            return path;
+        }
     }
 
     public void OnGUI()
@@ -57,7 +63,7 @@ class AlembicMaterialEditor : EditorWindow
         if (GUI.Button(new Rect(r.x + lw + hs + fw + hs, r.y, bw, r.height), "..."))
         {
             string startFolder = (xmlPath.Length > 0 ? Path.GetDirectoryName(xmlPath) : Application.dataPath);
-            xmlPath = EditorUtility.OpenFilePanel("Select Material Assignment File", startFolder, "xml");
+            xmlPath = MakeDataRelativePath(EditorUtility.OpenFilePanel("Select Material Assignment File", startFolder, "xml"));
 
             if (xmlPath.Length > 0 && materialFolder.Length == 0)
             {
@@ -77,7 +83,7 @@ class AlembicMaterialEditor : EditorWindow
         if (GUI.Button(new Rect(r.x + lw + hs + fw + hs, r.y, bw, r.height), "..."))
         {
             string startFolder = (materialFolder.Length > 0 ? materialFolder : Application.dataPath);
-            materialFolder = EditorUtility.OpenFolderPanel("Select Materials Folder", startFolder, "");
+            materialFolder = MakeDataRelativePath(EditorUtility.OpenFolderPanel("Select Materials Folder", startFolder, ""));
         }
 
         // Target asset node controls
@@ -95,295 +101,12 @@ class AlembicMaterialEditor : EditorWindow
 
         if (GUI.Button(new Rect(r.x, r.y, bw, r.height), "Import"))
         {
-            Uri tmp = new Uri(Application.dataPath + "/");
-
-            string relMatFolder = "Assets/" + tmp.MakeRelativeUri(new Uri(materialFolder)).ToString();
-
-            Import(xmlPath, assetRoot, relMatFolder);
+            AlembicMaterial.Import(Application.dataPath + "/" + xmlPath, assetRoot, "Assets/" + materialFolder); //relMatFolder);
         }
 
         if (GUI.Button(new Rect(r.x + bw + hs, r.y, bw, r.height), "Export"))
         {
-            Export(xmlPath, assetRoot);
+            AlembicMaterial.Export(Application.dataPath + "/" + xmlPath, assetRoot);
         }
-    }
-
-    static GameObject SearchNodeInstance(Transform parent, string name, int instNum, ref int curInst)
-    {
-        Transform node = parent.FindChild (name);
-
-        if (node != null)
-        {
-            if (curInst == instNum)
-            {
-                return node.gameObject;
-            }
-            else
-            {
-                ++curInst;
-            }
-        }
-
-        for (int i = 0; i < parent.childCount; ++i)
-        {
-            GameObject rv = SearchNodeInstance(parent.GetChild(i), name, instNum, ref curInst);
-
-            if (rv != null)
-            {
-                return rv;
-            }
-        }
-
-        return null;
-    }
-  
-    static GameObject FindNode(GameObject root, string path, int instNum)
-    {
-        if (root == null)
-        {
-            return null;
-        }
-
-        string[] paths = path.Split(PathSep, StringSplitOptions.RemoveEmptyEntries);
-
-        if (paths.Length == 0)
-        {
-            return null;
-        }
-
-        bool isFullPath = path.StartsWith("/");
-
-        if (isFullPath)
-        {
-            int curPath = 0;
-
-            Transform curNode = root.transform;
-
-            while (curNode != null && curPath < paths.Length)
-            {
-                Transform child = curNode.FindChild(paths[curPath]);
-
-                if (child == null)
-                {
-                    Debug.Log("Object \"" + curNode.name + "\" has no child named \"" + paths[curPath] + "\"");
-                    curNode = null;
-                    break;
-                }
-                else
-                {
-                    curNode = child;
-                    ++curPath;
-                }
-            }
-
-            if (curNode == null)
-            {
-                Debug.Log("Failed to find object \"" + path + "\"");
-                return null;
-            }
-            else
-            {
-                return curNode.gameObject;
-            }
-        }
-        else
-        {
-            string name = path;
-            int curInst = 0;
-
-            int idx = name.LastIndexOf("/");
-            if (idx >= 0 && idx < path.Length)
-            {
-                name = name.Substring(idx + 1);
-            }
-
-            if (instNum < 0)
-            {
-                instNum = 0;
-            }
-
-            return SearchNodeInstance(root.transform, name, instNum, ref curInst);
-        }
-    }
-  
-    static Material GetMaterial(string name, string matfolder)
-    {
-        string[] guids = AssetDatabase.FindAssets(name + " t:material", new string[1] { matfolder });
-
-        if (guids.Length == 1)
-        {
-            return AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(guids[0]), typeof(Material)) as Material;
-        }
-        else
-        {
-            return null;
-        }
-    }
-
-    static void Import(string xmlPath, GameObject root, string materialFolder)
-    {
-        //AlembicMaterial[] mal = root.GetComponentsInChildren<AlembicMaterial>();
-        //if (mal.Length > 0)
-        //{
-        //  foreach (AlembicMaterial ma in mal)
-        //  {
-        //    Component.DestroyImmediate(ma);
-        //  }
-        //}
-
-        XmlDocument doc = new XmlDocument();
-        doc.Load(xmlPath);
-        
-        XmlNode xmlRoot = doc.DocumentElement;
-        
-        XmlNodeList nodes = xmlRoot.SelectNodes("/assignments/node");
-
-        Dictionary<GameObject, List<AlembicMaterial.Assignment> > all_assignments = new Dictionary<GameObject, List<AlembicMaterial.Assignment> >();
-
-        foreach (XmlNode node in nodes)
-        {
-            string path = node.Attributes["path"].Value;
-
-            XmlNodeList shaders = node.SelectNodes("shader");
-
-            foreach (XmlNode shader in shaders)
-            {
-                XmlAttribute name = shader.Attributes["name"];
-                XmlAttribute inst = shader.Attributes["instance"];
-
-                if (name == null)
-                {
-                    continue;
-                }
-
-                int instNum = (inst == null ? 0 : Convert.ToInt32(inst.Value));
-
-                GameObject target = FindNode(root, path, instNum);
-                
-                if (target == null)
-                {
-                    continue;
-                }
-                
-                MeshFilter mesh = target.GetComponent<MeshFilter>();
-
-                if (mesh == null)
-                {
-                    continue;
-                }
-
-                Renderer renderer = target.GetComponent<Renderer>();
-
-                if (renderer == null)
-                {
-                    continue;
-                }
-
-                List<AlembicMaterial.Assignment> assignments;
-
-                if (!all_assignments.ContainsKey(target))
-                {
-                    assignments = new List<AlembicMaterial.Assignment>();
-                    all_assignments.Add(target, assignments);
-                }
-                else
-                {
-                    assignments = all_assignments[target];
-                }
-
-                Material material = GetMaterial(name.Value, materialFolder);
-
-                if (material == null)
-                {
-                    material = new Material(Shader.Find("Standard"));
-
-                    material.color = new Color(UnityEngine.Random.value,
-                                               UnityEngine.Random.value,
-                                               UnityEngine.Random.value);
-                    
-                    AssetDatabase.CreateAsset(material, materialFolder + "/" + name.Value + ".mat");
-                }
-
-                // Get or create material assignment
-                bool newlyAssigned = false;
-                AlembicMaterial.Assignment a = null;
-                
-                if (a == null)
-                {
-                    a = new AlembicMaterial.Assignment();
-                    a.material = material;
-                    a.faces = new List<int>();
-
-                    assignments.Add(a);
-
-                    newlyAssigned = true;
-                }
-
-                string faceset = shader.InnerText;
-                faceset.Trim();
-
-                if (faceset.Length > 0 && (newlyAssigned || a.faces.Count > 0))
-                {
-                    string[] items = faceset.Split(FaceSep, StringSplitOptions.RemoveEmptyEntries);
-
-                    for (int i=0; i<items.Length; ++i)
-                    {
-                        string[] rng = items[i].Split(RangeSep, StringSplitOptions.RemoveEmptyEntries);
-
-                        if (rng.Length == 1)
-                        {
-                            a.faces.Add(Convert.ToInt32(rng[0]));
-                        }
-                        else if (rng.Length == 2)
-                        {
-                            int j0 = Convert.ToInt32(rng[0]);
-                            int j1 = Convert.ToInt32(rng[1]);
-
-                            for (int j=j0; j<=j1; ++j)
-                            {
-                                a.faces.Add(j);
-                            }
-                        }
-                    }
-
-                    if (!newlyAssigned)
-                    {
-                        a.faces = new List<int>(new HashSet<int>(a.faces));
-                    }
-                }
-                else if (faceset.Length == 0 && a.faces.Count > 0)
-                {
-                    // Shader assgined to whole object, remove any face level assignments
-                    a.faces.Clear();
-                }
-            }
-        }
-
-        // Update AlembicMaterial components
-        foreach (KeyValuePair<GameObject, List<AlembicMaterial.Assignment> > pair in all_assignments)
-        {
-            AlembicMaterial abcmaterial = pair.Key.GetComponent<AlembicMaterial>();
-
-            if (abcmaterial == null)
-            {
-                abcmaterial = pair.Key.AddComponent<AlembicMaterial>();
-            }
-
-            abcmaterial.UpdateAssignments(pair.Value);
-        }
-
-        // Force refresh
-        AlembicStream abcstream = root.GetComponent<AlembicStream>();
-            
-        if (abcstream != null)
-        {
-            abcstream.m_force_refresh = true;
-        }
-    }
-
-    static void Export(string xmlPath, GameObject root)
-    {
-        // Not Yet Implemented
-        Debug.Log("Material assignment export not yet implemented");
     }
 }
