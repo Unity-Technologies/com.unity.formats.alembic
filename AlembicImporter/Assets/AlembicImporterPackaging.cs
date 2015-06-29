2767dc7896213dec47a5f01aa7868027be2906a3
i-saint <saint.skr@gmail.com>
2015-06-29 23:37:04 +0900

    added package

---
+++ AlembicImporter/Assets/AlembicImporterPackaging.cs
@@ -0,0 +1,25 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEditor;
+
+
+public class AlembicImporterPackaging
+{
+    [MenuItem("Assets/AlembicImporter/MakePackage")]
+    public static void MakePackage()
+    {
+        string[] files = new string[]
+        {
+"Assets/AlembicImporter/Scripts",
+"Assets/Plugins",
+        };
+        AssetDatabase.ExportPackage(files, "AlembicImporter.unitypackage", ExportPackageOptions.Recurse);
+    }
+
+}
+#endif // UNITY_EDITOR
