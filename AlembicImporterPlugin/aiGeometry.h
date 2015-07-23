20d38820dca7f8e3e19d9304cad743068af4f03a
gatgui <gatgui@gmail.com>
2015-07-23 12:32:22 +0900

    remove unused code

--- AlembicImporterPlugin/aiGeometry.h
+++ AlembicImporterPlugin/aiGeometry.h
@@ -71,49 +71,18 @@ public:
     bool        hasNormals() const;
     bool        hasUVs() const;
 
-    uint32_t    getIndexCount() const;
-    uint32_t    getVertexCount() const;
-    void        copyIndices(int *dst) const;
-    void        copyVertices(abcV3 *dst) const;
-    void        copyNormals(abcV3 *dst) const;
-    void        copyUVs(abcV2 *dst) const;
-
-    bool        getSplitedMeshInfo(aiSplitedMeshInfo &sp, const aiSplitedMeshInfo& prev, int maxVertices) const;
-    void        copySplitedIndices(int *dst, const aiSplitedMeshInfo &smi) const;
-    void        copySplitedVertices(abcV3 *dst, const aiSplitedMeshInfo &smi) const;
-    void        copySplitedNormals(abcV3 *dst, const aiSplitedMeshInfo &smi) const;
-    void        copySplitedUVs(abcV2 *dst, const aiSplitedMeshInfo &smi) const;
-
     uint32_t    getSplitCount() const;
     uint32_t    getSplitCount(bool forceRefresh);
+    
     uint32_t    getVertexBufferLength(uint32_t splitIndex) const;
     void        fillVertexBuffer(uint32_t splitIndex, abcV3 *positions, abcV3 *normals, abcV2 *uvs, abcV4 *T);
+    
     uint32_t    prepareSubmeshes(const aiFacesets *facesets);
     uint32_t    getSplitSubmeshCount(uint32_t splitIndex) const;
     bool        getNextSubmesh(aiSubmeshInfo &smi);
     void        fillSubmeshIndices(int *dst, const aiSubmeshInfo &smi) const;
 
 private:
-
-    template <typename NormalIndexArray>
-    void copySplitedNormals(abcV3 *dst, const NormalIndexArray &indices, const aiSplitedMeshInfo &smi, float xScale) const
-    {
-        const auto &counts = *m_counts;
-        const auto &normals = *m_normals.getVals();
-        
-        uint32_t a = 0;
-        
-        for (int fi = 0; fi < smi.numFaces; ++fi)
-        {
-            int ngon = counts[smi.beginFace + fi];
-            for (int ni = 0; ni < ngon; ++ni)
-            {
-                dst[a + ni] = normals[indices[a + ni + smi.beginIndex]];
-                dst[a + ni].x *= xScale;
-            }
-            a += ngon;
-        }
-    }
 
     void updateSplits();
     bool updateUVs(Abc::ISampleSelector &ss);
@@ -131,14 +100,14 @@ private:
     typedef std::set<size_t> Faceset;
     typedef std::vector<Faceset> Facesets;
     
-    struct SubmeshID
+    struct SubmeshKey
     {
         int uTile;
         int vTile;
         int facesetIndex;
         int splitIndex;
 
-        inline SubmeshID(float u, float v, int fi=-1, int si=0)
+        inline SubmeshKey(float u, float v, int fi=-1, int si=0)
             : uTile((int)floor(u))
             , vTile((int)floor(v))
             , facesetIndex(fi)
@@ -146,31 +115,18 @@ private:
         {
         }
 
-        inline bool operator<(const SubmeshID &rhs) const
+        inline bool operator<(const SubmeshKey &rhs) const
         {
-            if (splitIndex < rhs.splitIndex)
-            {
-                return true;
-            }
-            else if (splitIndex == rhs.splitIndex)
-            {
-                if (facesetIndex < rhs.facesetIndex)
-                {
-                    return true;
-                }
-                else if (facesetIndex == rhs.facesetIndex)
-                {
-                    if (uTile < rhs.uTile)
-                    {
-                        return true;
-                    }
-                    else if (uTile == rhs.uTile)
-                    {
-                        return (vTile < rhs.vTile);
-                    }
-                }
-            }
-            return false;
+            if (splitIndex < rhs.splitIndex) return true;
+            if (splitIndex > rhs.splitIndex) return false;
+
+            if (facesetIndex < rhs.facesetIndex) return true;
+            if (facesetIndex > rhs.facesetIndex) return false;
+
+            if (uTile < rhs.uTile) return true;
+            if (uTile > rhs.uTile) return false;
+
+            return (vTile < rhs.vTile);
         }
     };
 
