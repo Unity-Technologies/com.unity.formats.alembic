fcd109903d093ae27662b76695b048e69d1a0c75
jasonm-unity <jasonm@unity3d.com>
2017-02-08 10:53:35 -0500

    Upgraded Alembic Importer Inspector

--- AlembicImporter/Assets/UTJ/AlembicImporter/Editor/AlembicImporterEditor.cs
+++ AlembicImporter/Assets/UTJ/AlembicImporter/Editor/AlembicImporterEditor.cs
@@ -9,28 +9,54 @@ namespace UTJ.Alembic
 	[CustomEditor(typeof(AlembicImporter))]
 	public class AlembicImporterEditor : ScriptedImporterInspector
 	{
+
 		public override void OnInspectorGUI()
 		{
-			var property = this.serializedObject.FindProperty("m_importMode");
-			AddEnumPopup(property, "Import Mode", typeof(AlembicImportMode));
+			var importer = serializedObject.targetObject as AlembicImporter;
+			var settings = importer.m_ImportSettings;
+			var serSettings = serializedObject.FindProperty(() => importer.m_ImportSettings);
 
-			base.OnInspectorGUI();
+			AddEnumProperty(serializedObject.FindProperty(() => importer.m_importMode), "Import Mode", "", typeof(AlembicImportMode));
+
+			AddBoolProperty(serSettings.FindPropertyRelative(() => settings.m_swapHandedness), "Swap handedness", "");
+			AddBoolProperty(serSettings.FindPropertyRelative(() => settings.m_swapFaceWinding), "Swap face winding", "");
+			AddBoolProperty(serSettings.FindPropertyRelative(() => settings.m_submeshPerUVTile), "Submesh per UV tile", "");
+
+			AddEnumProperty(serSettings.FindPropertyRelative(() => settings.m_normalsMode), "Normals mode", "", settings.m_normalsMode.GetType());
+			AddEnumProperty(serSettings.FindPropertyRelative(() => settings.m_tangentsMode), "Tangent mode", "", settings.m_tangentsMode.GetType());
+			AddEnumProperty(serSettings.FindPropertyRelative(() => settings.m_aspectRatioMode), "Aspect ratio mode", "", settings.m_aspectRatioMode.GetType());
+			AddBoolProperty(serSettings.FindPropertyRelative(() => settings.m_useThreads), "Use threads", "");
+			AddIntProperty(serSettings.FindPropertyRelative(() => settings.m_sampleCacheSize), "Sample cache size", "");
+			
+			base.ApplyRevertGUI();
 		}
 
-		void AddEnumPopup(SerializedProperty porperty, string text, Type typeOfEnum)
+		private void AddBoolProperty(SerializedProperty porperty, string text, string tooltip)
+		{
+			var orgValue = porperty.boolValue;
+			var newValue = EditorGUILayout.Toggle(new GUIContent(text, tooltip), orgValue);
+			porperty.boolValue = newValue;
+		}
+
+
+		void AddEnumProperty(SerializedProperty porperty, string text, string tooltip, Type typeOfEnum)
 		{
 			Rect ourRect = EditorGUILayout.BeginHorizontal();
 			EditorGUI.BeginProperty(ourRect, GUIContent.none, porperty);
-			EditorGUI.BeginChangeCheck();
 
-			int actualSelected = 1;
 			int selectionFromInspector = porperty.intValue;
 			string[] enumNamesList = System.Enum.GetNames(typeOfEnum);
-			actualSelected = EditorGUILayout.Popup(text, selectionFromInspector, enumNamesList);
+			var actualSelected = EditorGUILayout.Popup(text, selectionFromInspector, enumNamesList);
 			porperty.intValue = actualSelected;
-
 			EditorGUI.EndProperty();
 			EditorGUILayout.EndHorizontal();
+		}
+
+		void AddIntProperty(SerializedProperty porperty, string text, string tooltip)
+		{
+			var orgValue = porperty.intValue;
+			var newValue = EditorGUILayout.IntField(new GUIContent(text, tooltip), orgValue);
+			porperty.intValue = newValue;
 		}
 
 	}
