f80c7ea3bc8dc6c4ae2ebbbd1ba57093cc9e5ad7
i-saint <saint.skr@gmail.com>
2018-02-16 05:27:26 +0900

    AlembicTextureAnimator

--- AlembicImporter/Assets/Tests/Scripts/RandomWait.cs
+++ AlembicImporter/Assets/Tests/Scripts/RandomWait.cs
@@ -5,12 +5,12 @@ namespace UTJ.Alembic
 {
     public class RandomWait : MonoBehaviour
     {
-        public float m_minWait;
-        public float m_maxWait;
+        public float m_minWaitMillisec;
+        public float m_maxWaitMillisec;
 
         void LateUpdate()
         {
-            var t = Random.Range(m_minWait, m_maxWait);
+            var t = Random.Range(m_minWaitMillisec, m_maxWaitMillisec);
             System.Threading.Thread.Sleep((int)t);
         }
     }
