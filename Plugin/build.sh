1ca21e919bc6bf8aafff925631695f03db040080
Benoit Hudson <benoit.hudson@imaginary-spaces.com>
2018-02-26 11:11:35 -0500

    Drop the recommendation to 'make install'
    What needs to be installed is installed by 'make' already.

--- Plugin/build.sh
+++ Plugin/build.sh
@@ -24,7 +24,7 @@ plugindir=`cd "$plugindir" && pwd`
 # echo and exit on error, like a makefile
 set -ex
 
-# build Alembic
+# build & install Alembic
 mkdir alembic-build
 pushd alembic-build
 cmake "$alembic" -DUSE_HDF5=ON -DALEMBIC_SHARED_LIBS=OFF -DSTATIC_HDF5=ON -DCMAKE_INSTALL_PREFIX="`pwd`/../alembic-installed"
@@ -33,16 +33,11 @@ make test
 make install
 popd
 
-# build abci
+# build & install abci ('make' installs into the local
+# com.unity.alembic package)
 mkdir plugin-build
 pushd plugin-build
 cmake "$plugindir" -DALEMBIC_DIR="`pwd`/../alembic-installed"
 make -j4
+make test
 popd
-
-# recommend some work to do
-set +ex
-
-echo "If you're happy with the results, run this lines:"
-echo ""
-echo "  cd plugin-build && make install"
