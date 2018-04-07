39c3a49eb7bb24a876ef0ba838097bda3bc1137f
i-saint <saint.skr@gmail.com>
2018-04-08 07:16:48 +0900

    setup.bat (libAlembic 1.7.5 -> 1.7.7)

--- Plugin/setup.bat
+++ Plugin/setup.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo "downloading external libararies..."
-powershell.exe -Command "[System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]::Tls12; wget https://github.com/unity3d-jp/AlembicImporter/releases/download/20180122/External.7z -OutFile External/External.7z"
+powershell.exe -Command "[System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]::Tls12; wget https://github.com/unity3d-jp/AlembicImporter/releases/download/20180320/External.7z -OutFile External/External.7z"
 cd External
 7z\7za.exe x -aos External.7z
 cd ..
