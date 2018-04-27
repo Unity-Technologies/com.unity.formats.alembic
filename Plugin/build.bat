888799a913ee9cf1ac1f6feb01d969d0693dc35a
i-saint <saint.skr@gmail.com>
2018-04-27 15:00:57 +0900

    improve build script build script

--- Plugin/build.bat
+++ Plugin/build.bat
@@ -1,3 +1,13 @@
 call buildtools.bat
+
 msbuild abci.sln /t:Build /p:Configuration=Master /p:Platform=Win32 /m /nologo
+IF %ERRORLEVEL% NEQ 0 (
+    pause
+    exit /B 1
+)
+
 msbuild abci.sln /t:Build /p:Configuration=Master /p:Platform=x64 /m /nologo
+IF %ERRORLEVEL% NEQ 0 (
+    pause
+    exit /B 1
+)
