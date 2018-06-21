fe32c3e212dc8b85a4de868c3e6515866f53e884
vkovec <viktoria.kovecses@gmail.com>
2018-06-21 14:48:21 -0400

    added simple export test

--- AlembicImporter/Assets/UnitTests/AlembicExportTest.cs
+++ AlembicImporter/Assets/UnitTests/AlembicExportTest.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Formats.Alembic.Exporter;
+using System.IO;
 
 namespace UnityEditor.Formats.Alembic.Exporter.UnitTests
 {
@@ -11,16 +13,41 @@ namespace UnityEditor.Formats.Alembic.Exporter.UnitTests
         private string m_pathToScene;
 
         [Test]
-        public void TestBasics()
+        public void TestOneShotExport()
         {
             // open scene
+            var sceneSource = Application.dataPath + "/Tests/TestCloth.unity";
+            SceneManagement.EditorSceneManager.OpenScene(sceneSource, SceneManagement.OpenSceneMode.Single);
+            
+            // export one shot
+            var alembicExporter = Object.FindObjectOfType<AlembicExporter>();
+            Assert.That(alembicExporter, Is.Not.Null);
+
+            var exportPath = Application.dataPath + "/Temp/test.abc";
+
+            alembicExporter.recorder.settings.OutputPath = exportPath;
+
+            alembicExporter.OneShot();
+
+            Assert.That(string.IsNullOrEmpty(exportPath), Is.False);
+            Assert.That(File.Exists(exportPath));
+        }
+
+        [Test]
+        public void TestClothExport()
+        {
+            // open scene
+            var sceneSource = Application.dataPath + "/Tests/TestCloth.unity";
+            SceneManagement.EditorSceneManager.OpenScene(sceneSource, SceneManagement.OpenSceneMode.Single);
 
             // start playing
+            EditorApplication.isPlaying = true;
 
             // begin recording
             // end recording
 
             // end playing
+            EditorApplication.isPlaying = false;
 
             // check fbx was created in expected location
             // check fbx is imported + is not empty
