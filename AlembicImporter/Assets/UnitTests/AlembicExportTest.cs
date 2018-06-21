da8a952af83c56ff304c2823751d4a93dbffa00d
vkovec <viktoria.kovecses@gmail.com>
2018-06-21 11:53:28 -0400

    add empty unit test

---
+++ AlembicImporter/Assets/UnitTests/AlembicExportTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityEditor.Formats.Alembic.Exporter.UnitTests
+{
+    public class AlembicExportTest
+    {
+        private string m_pathToScene;
+
+        [Test]
+        public void TestBasics()
+        {
+            // open scene
+
+            // start playing
+
+            // begin recording
+            // end recording
+
+            // end playing
+
+            // check fbx was created in expected location
+            // check fbx is imported + is not empty
+        }
+    }
+}
