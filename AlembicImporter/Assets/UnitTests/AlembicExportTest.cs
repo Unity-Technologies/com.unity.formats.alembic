2383229f6b7d4f2a3c2dd2837bb3a1cabe031cf3
vkovec <viktoria.kovecses@gmail.com>
2018-06-22 11:59:38 -0400

    add test base for creating random file paths
    + other helper functions
    + clean up code

--- AlembicImporter/Assets/UnitTests/AlembicExportTest.cs
+++ AlembicImporter/Assets/UnitTests/AlembicExportTest.cs
@@ -7,9 +7,143 @@ using System.IO;
 
 namespace UnityEditor.Formats.Alembic.Exporter.UnitTests
 {
-    public class AlembicExportTest
+    public class AlembicTestBase
     {
-        private string m_pathToScene;
+        // --- Helpers for creating temporary paths -----
+        private string _testDirectory;
+        protected string filePath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_testDirectory))
+                {
+                    // Create a directory in the asset path.
+                    _testDirectory = GetRandomFileNamePath("Assets", extName: "");
+                    System.IO.Directory.CreateDirectory(_testDirectory);
+                }
+                return _testDirectory;
+            }
+        }
+
+        private string _fileNamePrefix;
+        protected string fileNamePrefix
+        {
+            get { return string.IsNullOrEmpty(_fileNamePrefix) ? "_safe_to_delete__" : _fileNamePrefix; }
+            set { _fileNamePrefix = value; }
+        }
+
+        private string _fileNameExt;
+        protected string fileNameExt { get { return string.IsNullOrEmpty(_fileNameExt) ? ".fbx" : _fileNameExt; } set { _fileNameExt = value; } }
+
+        private string MakeFileName(string baseName = null, string prefixName = null, string extName = null)
+        {
+            if (baseName == null)
+            {
+                // GetRandomFileName makes a random 8.3 filename
+                // We don't want the extension.
+                baseName = Path.GetFileNameWithoutExtension(Path.GetRandomFileName());
+            }
+
+            if (prefixName == null)
+                prefixName = this.fileNamePrefix;
+
+            if (extName == null)
+                extName = this.fileNameExt;
+
+            return prefixName + baseName + extName;
+        }
+
+        /// <summary>
+        /// Create a random path for a file.
+        ///
+        /// By default the pathName is null, which defaults to a particular
+        /// folder in the Assets folder that will be deleted on termination of
+        /// this test.
+        ///
+        /// By default the prefix is a fixed string. You can set it differently if you care.
+        ///
+        /// By default the extension is ".fbx". You can set it differently if you care.
+        ///
+        /// By default we use platform path separators. If you want a random
+        /// asset path e.g. for AssetDatabase.LoadMainAssetAtPath or for
+        /// PrefabUtility.CreatePrefab, you need to use the '/' as separator
+        /// (even on windows).
+        ///
+        /// See also convenience functions like:
+        ///     GetRandomPrefabAssetPath()
+        ///     GetRandomFbxFilePath()
+        /// </summary>
+        protected string GetRandomFileNamePath(
+                string pathName = null,
+                string prefixName = null,
+                string extName = null,
+                bool unityPathSeparator = false)
+        {
+            string temp;
+
+            if (pathName == null)
+            {
+                pathName = this.filePath;
+            }
+
+            // repeat until you find a file that does not already exist
+            do
+            {
+                temp = Path.Combine(pathName, MakeFileName(prefixName: prefixName, extName: extName));
+            } while (File.Exists(temp));
+
+            // Unity asset paths need a slash on all platforms.
+            if (unityPathSeparator)
+            {
+                temp = temp.Replace('\\', '/');
+            }
+
+            return temp;
+        }
+
+        /// <summary>
+        /// Return a random .abc path that you can use in
+        /// the File APIs.
+        /// </summary>
+        protected string GetRandomAbcFilePath()
+        {
+            return GetRandomFileNamePath(extName: ".abc", unityPathSeparator: false);
+        }
+        // -------------------------------------------------
+
+
+        [TearDown]
+        public virtual void Term()
+        {
+            if (string.IsNullOrEmpty(_testDirectory))
+            {
+                return;
+            }
+
+            // Delete the directory on the next editor update.  Otherwise,
+            // prefabs don't get deleted and the directory delete fails.
+            EditorApplication.update += DeleteOnNextUpdate;
+        }
+
+        // Helper for the tear-down. This is run from the editor's update loop.
+        void DeleteOnNextUpdate()
+        {
+            EditorApplication.update -= DeleteOnNextUpdate;
+            try
+            {
+                Directory.Delete(filePath, recursive: true);
+                AssetDatabase.Refresh();
+            }
+            catch (IOException)
+            {
+                // ignore -- something else must have deleted this.
+            }
+        }
+    }
+
+    public class AlembicExportTest : AlembicTestBase
+    {
+        private const string c_scene = "TestCloth";
 
         private AlembicExporter GetAlembicExporter()
         {
@@ -18,38 +152,52 @@ namespace UnityEditor.Formats.Alembic.Exporter.UnitTests
             return alembicExporter;
         }
 
+        private string GetAssetsAbsolutePath(string relPath)
+        {
+            return Application.dataPath + relPath.Replace("Assets", "");
+        }
+
+        /// <summary>
+        /// Test that the abc file was reimported and can be loaded into Unity through
+        /// the asset database.
+        /// </summary>
+        /// <param name="abcPath"></param>
+        private void TestAbcImported(string abcPath)
+        {
+            AssetDatabase.Refresh();
+
+            var absPath = GetAssetsAbsolutePath(abcPath);
+
+            Assert.That(string.IsNullOrEmpty(absPath), Is.False);
+            Assert.That(File.Exists(absPath));
+
+            // now try loading the asset to see if it imported properly
+            var obj = AssetDatabase.LoadMainAssetAtPath(abcPath);
+            Assert.That(obj, Is.Not.Null);
+            var go = obj as GameObject;
+            Assert.That(go, Is.Not.Null);
+        }
+
         [UnityTest]
         public IEnumerator TestOneShotExport()
         {
             // open scene
-            var sceneSource = "TestCloth";
-            SceneManagement.EditorSceneManager.LoadScene(sceneSource, UnityEngine.SceneManagement.LoadSceneMode.Single);
+            SceneManagement.EditorSceneManager.LoadScene(c_scene, UnityEngine.SceneManagement.LoadSceneMode.Single);
 
             // yield once while scene loads
             yield return null;
-            Debug.Log("begin testing");
 
             // export one shot
             var alembicExporter = GetAlembicExporter();
-            alembicExporter.captureOnStart = false;
+            var exportPath = GetRandomAbcFilePath();
 
-            var exportPath = Application.dataPath + "/Temp/test.abc";
-
-            alembicExporter.recorder.settings.OutputPath = exportPath;
+            alembicExporter.recorder.settings.OutputPath = GetAssetsAbsolutePath(exportPath);
 
             alembicExporter.OneShot();
 
             yield return null;
-            AssetDatabase.Refresh();
 
-            Assert.That(string.IsNullOrEmpty(exportPath), Is.False);
-            Assert.That(File.Exists(exportPath));
-
-            // now try loading the asset to see if it imported properly
-            var obj = AssetDatabase.LoadMainAssetAtPath("Assets/Temp/test.abc");
-            Assert.That(obj, Is.Not.Null);
-            var go = obj as GameObject;
-            Assert.That(go, Is.Not.Null);
+            TestAbcImported(exportPath);
 
             yield return null;
         }
@@ -58,17 +206,15 @@ namespace UnityEditor.Formats.Alembic.Exporter.UnitTests
         public IEnumerator TestClothExport()
         {
             // open scene
-            var sceneSource = "TestCloth";// Application.dataPath + "/Tests/TestCloth.unity";
-            SceneManagement.EditorSceneManager.LoadScene(sceneSource, UnityEngine.SceneManagement.LoadSceneMode.Single);
+            SceneManagement.EditorSceneManager.LoadScene(c_scene, UnityEngine.SceneManagement.LoadSceneMode.Single);
 
             // yield once while scene loads
             yield return null;
 
             var alembicExporter = GetAlembicExporter();
+            var exportPath = GetRandomAbcFilePath();
 
-            var exportPath = Application.dataPath + "/Temp/test2.abc";
-
-            alembicExporter.recorder.settings.OutputPath = exportPath;
+            alembicExporter.recorder.settings.OutputPath = GetAssetsAbsolutePath(exportPath);
             alembicExporter.maxCaptureFrame = 300;
 
             alembicExporter.BeginRecording();
@@ -82,11 +228,8 @@ namespace UnityEditor.Formats.Alembic.Exporter.UnitTests
             {
                 yield return null;
             }
-            Debug.Log("exported file");
 
-            // check fbx was created in expected location
-            // check fbx is imported + is not empty
-            yield return null;
+            TestAbcImported(exportPath);
         }
     }
 }
