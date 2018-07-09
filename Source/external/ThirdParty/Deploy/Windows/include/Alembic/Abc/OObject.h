05650d46b778987d27c5588996019aaacc256ddb
Mark Visser <markv@unity3d.com>
2018-07-09 17:22:35 -0400

    added Windows dependencies

---
+++ Source/external/ThirdParty/Deploy/Windows/include/Alembic/Abc/OObject.h
@@ -0,0 +1,264 @@
+//-*****************************************************************************
+//
+// Copyright (c) 2009-2013,
+//  Sony Pictures Imageworks, Inc. and
+//  Industrial Light & Magic, a division of Lucasfilm Entertainment Company Ltd.
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+// *       Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// *       Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+// *       Neither the name of Sony Pictures Imageworks, nor
+// Industrial Light & Magic nor the names of their contributors may be used
+// to endorse or promote products derived from this software without specific
+// prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//-*****************************************************************************
+
+#ifndef _Alembic_Abc_OObject_h_
+#define _Alembic_Abc_OObject_h_
+
+#include <Alembic/Util/Export.h>
+#include <Alembic/Abc/Foundation.h>
+#include <Alembic/Abc/Base.h>
+#include <Alembic/Abc/Argument.h>
+#include <Alembic/Abc/OArchive.h>
+
+namespace Alembic {
+namespace Abc {
+namespace ALEMBIC_VERSION_NS {
+
+class OCompoundProperty;
+
+//-*****************************************************************************
+class ALEMBIC_EXPORT OObject : public Base
+{
+public:
+    //! By convention, we always define "this_type" in every Abc
+    //! class. This convention is relied upon by the unspecified-bool-type
+    //! conversion.
+    typedef OObject this_type;
+    typedef OObject operator_bool_base_type;
+
+    //-*************************************************************************
+    // CONSTRUCTION, DESTRUCTION, ASSIGNMENT
+    //-*************************************************************************
+
+    //! The default constructor creates an empty OObject function set.
+    //! ...
+    OObject() {}
+
+    //! This creates a new object writer.
+    //! The first argument is the parent OObject from which the error handler
+    //! policy for inheritance is derived.  The remaining optional arguments
+    //! can be used to override the ErrorHandlerPolicy, and MetaData
+    OObject( OObject iParentObject,
+             const std::string &iName,
+
+             const Argument &iArg0 = Argument(),
+             const Argument &iArg1 = Argument(),
+             const Argument &iArg2 = Argument() );
+
+    //! This attaches an OObject wrapper around an existing
+    //! ObjectWriterPtr, with an optional error handling policy.
+    //! Arguments can be used to set error handling policy.
+    OObject( AbcA::ObjectWriterPtr iPtr,
+             const Argument &iArg0 = Argument(),
+             const Argument &iArg1 = Argument(),
+             const Argument &iArg2 = Argument() )
+      : m_object( iPtr )
+    {
+        // Set the error handling policy
+        getErrorHandler().setPolicy(
+            GetErrorHandlerPolicy( iPtr, iArg0, iArg1, iArg2 ) );
+    }
+
+    // Deprecated in favor of the constructor above
+    OObject( AbcA::ObjectWriterPtr iPtr,
+             WrapExistingFlag /* iFlag */,
+             const Argument &iArg0 = Argument(),
+             const Argument &iArg1 = Argument(),
+             const Argument &iArg2 = Argument() )
+      : m_object( iPtr )
+    {
+        // Set the error handling policy
+        getErrorHandler().setPolicy(
+            GetErrorHandlerPolicy( iPtr, iArg0, iArg1, iArg2 ) );
+    }
+
+    //! This attaches an OObject wrapper around the top
+    //! object in an archive.
+    //! Arguments can be used to set error handling policy.
+    OObject( OArchive & iArchive,
+             const Argument &iArg0 = Argument(),
+             const Argument &iArg1 = Argument(),
+             const Argument &iArg2 = Argument() )
+    {
+        init( iArchive, iArg0, iArg1, iArg2 );
+    }
+
+    // deprecated in favor of the constructor above
+    OObject( OArchive & iArchive,
+             TopFlag /* iTop */,
+             const Argument &iArg0 = Argument(),
+             const Argument &iArg1 = Argument(),
+             const Argument &iArg2 = Argument() )
+    {
+        init( iArchive, iArg0, iArg1, iArg2 );
+    }
+
+    //! Default copy constructor used
+    //! Default assignment operator used.
+
+    //! Destructor
+    //! ...
+    virtual ~OObject();
+
+    //-*************************************************************************
+    // OBJECT WRITER FUNCTIONALITY
+    //-*************************************************************************
+
+    //! Return the object's header.
+    //! ...
+    const AbcA::ObjectHeader & getHeader() const;
+
+    //! This function returns the object's local name
+    //! ...
+    const std::string &getName() const
+    { return getHeader().getName(); }
+
+    //! This function returns the object's full (unique with the archive)
+    //! name
+    const std::string &getFullName() const
+    { return getHeader().getFullName(); }
+
+    //! This function returns the object's metadata.
+    //! ...
+    const AbcA::MetaData &getMetaData() const
+    { return getHeader().getMetaData(); }
+
+    //! This function returns the object's archive, handily
+    //! wrapped in an OArchive wrapper.
+    OArchive getArchive();
+
+    //! This function returns the object's parent, handily
+    //! wrapped in an OObject wrapper. If the object is the top
+    //! level object, the OObject returned will be NULL.
+    OObject getParent();
+
+    //! This function returns the number of child objects that
+    //! this object has. This may change as new children
+    //! are created for writing.
+    size_t getNumChildren();
+
+    //! This function returns the headers of each of the child
+    //! objects created so far.
+    const AbcA::ObjectHeader & getChildHeader( size_t i );
+
+    //! This function returns the header of a named object if it has
+    //! been created - even if the object no longer exists.
+    const AbcA::ObjectHeader * getChildHeader( const std::string &iName );
+
+    //! This returns the single top-level OCompoundProperty that exists
+    //! automatically as part of the object.
+    OCompoundProperty getProperties();
+
+    //-*************************************************************************
+    // ADVANCED TOOLS
+    // Unless you really know why you need to be using these next few
+    // functions, they're probably best left alone. These functions attempt
+    // to find a pointer to an existing writer, instead of creating a new one.
+    //-*************************************************************************
+
+    //! This function returns an OObject wrapped around a pointer
+    //! to an already created child. This is distinct from creating a new
+    //! OObject as a child. If the writer associated with this child no longer
+    //! exists, this function will return an empty OObject.
+    OObject getChild( size_t iChildIndex );
+
+    //! This function returns an OObject wrapped around a pointer
+    //! to an already created child. This is distinct from creating a new
+    //! OObject as a child. If the writer associated with this child no longer
+    //! exists, this function will return an empty OObject.
+    OObject getChild( const std::string &iChildName );
+
+    //!-************************************************************************
+    // PROXY METHODS
+    // An OObject can refer to another OObject. When read in, this instance
+    // will be represented by the target source hierarchy and its children.
+    //!-************************************************************************
+    bool addChildInstance( OObject iTarget, const std::string& iName );
+
+    //-*************************************************************************
+    // ABC BASE MECHANISMS
+    // These functions are used by Abc to deal with errors, rewrapping,
+    // and so on.
+    //-*************************************************************************
+
+    //! getPtr, as usual, returns a shared ptr to the
+    //! underlying AbcCoreAbstract object, in this case the
+    //! ObjectWriterPtr.
+    AbcA::ObjectWriterPtr getPtr() { return m_object; }
+    const AbcA::ObjectWriterPtr getPtr() const { return m_object; }
+
+    //! Reset returns this function set to an empty, default
+    //! state.
+    void reset() { m_object.reset(); Base::reset(); }
+
+    //! Valid returns whether this function set is
+    //! valid.
+    bool valid() const
+    {
+        return ( Base::valid() && m_object );
+    }
+
+    //! The unspecified-bool-type operator casts the object to "true"
+    //! if it is valid, and "false" otherwise.
+    ALEMBIC_OPERATOR_BOOL( valid() );
+
+private:
+
+    void init( OArchive & iArchive,
+               const Argument &iArg0,
+               const Argument &iArg1,
+               const Argument &iArg2 );
+
+    void init( AbcA::ObjectWriterPtr iParentObject,
+               const std::string &iName,
+               ErrorHandler::Policy iParentPolicy,
+
+               const Argument &iArg0,
+               const Argument &iArg1,
+               const Argument &iARg2 );
+
+protected:
+    AbcA::ObjectWriterPtr m_object;
+};
+
+} // End namespace ALEMBIC_VERSION_NS
+
+using namespace ALEMBIC_VERSION_NS;
+
+} // End namespace Abc
+} // End namespace Alembic
+
+#endif
---
+++ Source/external/ThirdParty/Deploy/Windows/include/Alembic/Util/Foundation.h
@@ -0,0 +1,308 @@
+//-*****************************************************************************
+//
+// Copyright (c) 2009-2015,
+//  Sony Pictures Imageworks Inc. and
+//  Industrial Light & Magic, a division of Lucasfilm Entertainment Company Ltd.
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+// *       Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// *       Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+// *       Neither the name of Industrial Light & Magic nor the names of
+// its contributors may be used to endorse or promote products derived
+// from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//-*****************************************************************************
+
+#ifndef _Alembic_Util_Foundation_h_
+#define _Alembic_Util_Foundation_h_
+
+#include <Alembic/Util/Config.h>
+
+#ifdef ALEMBIC_LIB_USES_BOOST
+#include <boost/type_traits.hpp>
+#include <boost/ref.hpp>
+#include <boost/format.hpp>
+#include <boost/smart_ptr.hpp>
+#include <boost/static_assert.hpp>
+#include <boost/utility.hpp>
+#include <boost/cstdint.hpp>
+#include <boost/array.hpp>
+#include <boost/operators.hpp>
+#include <boost/foreach.hpp>
+#include <boost/unordered_map.hpp>
+
+// tr1 is not available in older versions of Visual Studio i.e. _MSC_VER <= 1600
+#elif defined(ALEMBIC_LIB_USES_TR1)
+#include <tr1/memory>
+#include <tr1/unordered_map>
+
+// default to C++11
+#else
+#include <unordered_map>
+#endif
+
+#include <memory>
+
+#include <half.h>
+
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <exception>
+#include <limits>
+
+#include <list>
+#include <map>
+#include <string>
+#include <vector>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include <Alembic/Util/Export.h>
+
+#ifdef _MSC_VER
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+// avoid windows min/max predefined macro conflicts
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
+// needed for mutex stuff
+#include <Windows.h>
+#endif
+
+// needed for std min/max
+#include <algorithm>
+
+#ifndef ALEMBIC_VERSION_NS
+#define ALEMBIC_VERSION_NS v10
+#endif
+
+namespace Alembic {
+namespace Util {
+namespace ALEMBIC_VERSION_NS {
+
+// similiar to boost::noncopyable
+// explicitly hides copy construction and copy assignment
+class ALEMBIC_EXPORT noncopyable
+{
+protected:
+    noncopyable() {}
+    ~noncopyable() {}
+
+private:
+    noncopyable( const noncopyable& );
+    const noncopyable& operator=( const noncopyable& );
+};
+
+#ifdef ALEMBIC_LIB_USES_BOOST
+using boost::dynamic_pointer_cast;
+using boost::enable_shared_from_this;
+using boost::shared_ptr;
+using boost::static_pointer_cast;
+using boost::weak_ptr;
+using boost::unordered_map;
+
+#elif defined(ALEMBIC_LIB_USES_TR1)
+using std::tr1::dynamic_pointer_cast;
+using std::tr1::enable_shared_from_this;
+using std::tr1::shared_ptr;
+using std::tr1::static_pointer_cast;
+using std::tr1::weak_ptr;
+using std::tr1::unordered_map;
+
+#else
+using std::dynamic_pointer_cast;
+using std::enable_shared_from_this;
+using std::shared_ptr;
+using std::static_pointer_cast;
+using std::weak_ptr;
+using std::unordered_map;
+using std::unique_ptr;
+#endif
+
+#if defined(ALEMBIC_LIB_USES_BOOST) || defined(ALEMBIC_LIB_USES_TR1)
+
+// define a very simple scoped ptr since unique_ptr isn't consistently
+// available on boost versions.  Otherwise we could use boost::scoped_ptr
+// or the deprecated std::auto_ptr for tr1.
+template<typename T>
+class unique_ptr : noncopyable
+{
+public:
+    unique_ptr()
+    {
+        p = NULL;
+    }
+
+    unique_ptr( T* val ) : p(val)
+    {
+    }
+
+    ~unique_ptr()
+    {
+        if ( p )
+        {
+            delete p;
+        }
+    }
+
+    void reset( T* val )
+    {
+        if ( p )
+        {
+            delete p;
+        }
+        p = val;
+    }
+
+    T* operator->() const
+    {
+        return p;
+    }
+private:
+    T* p;
+};
+
+#endif
+
+// similiar to boost::totally_ordered
+// only need < and == operators and this fills in the rest
+template < class T >
+class totally_ordered
+{
+    friend bool operator > ( const T& x, const T& y )
+    {
+        return y < x;
+    }
+
+    friend bool operator <= ( const T& x, const T& y )
+    {
+        return !( y < x );
+    }
+
+    friend bool operator >= ( const T& x, const T& y )
+    {
+        return !( x < y );
+    }
+
+    friend bool operator != ( const T& x, const T& y )
+    {
+        return !( x == y );
+    }
+};
+
+// inspired by boost::mutex
+#ifdef _MSC_VER
+
+class mutex : noncopyable
+{
+public:
+    mutex()
+    {
+        m = CreateMutex( NULL, FALSE, NULL );
+    }
+
+    ~mutex()
+    {
+        CloseHandle( m );
+    }
+
+    void lock()
+    {
+        WaitForSingleObject( m, INFINITE );
+    }
+
+    void unlock()
+    {
+        ReleaseMutex( m );
+    }
+
+private:
+    HANDLE m;
+};
+
+#else
+
+
+class mutex : noncopyable
+{
+public:
+    mutex()
+    {
+        pthread_mutex_init( &m, NULL );
+    }
+
+    ~mutex()
+    {
+        pthread_mutex_destroy( &m );
+    }
+
+    void lock()
+    {
+        pthread_mutex_lock( &m );
+    }
+
+    void unlock()
+    {
+        pthread_mutex_unlock( &m );
+    }
+
+private:
+    pthread_mutex_t m;
+};
+
+#endif
+
+class scoped_lock : noncopyable
+{
+public:
+    scoped_lock( mutex & l ) : m( l )
+    {
+        m.lock();
+    }
+
+    ~scoped_lock()
+    {
+        m.unlock();
+    }
+
+private:
+    mutex & m;
+};
+
+} // End namespace ALEMBIC_VERSION_NS
+
+using namespace ALEMBIC_VERSION_NS;
+
+} // End namespace Util
+} // End namespace Alembic
+
+#endif
