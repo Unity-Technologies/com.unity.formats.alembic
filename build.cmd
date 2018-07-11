51a4315a078ff3d8e4a88d505cc237b9e37a3477
Mark Visser <markv@unity3d.com>
2018-07-11 10:47:34 -0400

    switched build to Visual Studio 14 2015 Win64

--- build.cmd
+++ build.cmd
@@ -3,5 +3,5 @@ SET installdir=%cd%/build/install
 echo %deploydir%
 md build
 cd build
-cmake .. -DALEMBIC_DIR=%deploydir% -DHDF5_USE_STATIC_LIBRARIES=ON -DHDF5_ROOT=%deploydir% -DUSE_STATIC=ON -DENABLE_DEPLOY=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%deploydir% -DCMAKE_INSTALL_PREFIX=%installdir% -G "Visual Studio 15 2017 Win64"
+cmake .. -DALEMBIC_DIR=%deploydir% -DHDF5_USE_STATIC_LIBRARIES=ON -DHDF5_ROOT=%deploydir% -DUSE_STATIC=ON -DENABLE_DEPLOY=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%deploydir% -DCMAKE_INSTALL_PREFIX=%installdir% -G "Visual Studio 14 2015 Win64"
 cmake --build . --target INSTALL --config Release
