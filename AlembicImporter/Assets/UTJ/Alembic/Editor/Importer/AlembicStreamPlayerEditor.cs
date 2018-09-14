d306dbf022418e04790873d64047243a0ac8420d
i-saint <saint.skr@gmail.com>
2018-09-15 07:05:07 +0900

    adapt to Unity 2018.3 change

--- AlembicImporter/Assets/UTJ/Alembic/Editor/Importer/AlembicStreamPlayerEditor.cs
+++ AlembicImporter/Assets/UTJ/Alembic/Editor/Importer/AlembicStreamPlayerEditor.cs
@@ -87,7 +87,12 @@ namespace UTJ.Alembic
                 EditorGUI.indentLevel--;
             }
 
+#if UNITY_2018_3_OR_NEWER
+            var prefabStatus = PrefabUtility.GetPrefabInstanceStatus(streamPlayer.gameObject);
+            if (prefabStatus == PrefabInstanceStatus.NotAPrefab || prefabStatus == PrefabInstanceStatus.Disconnected)
+#else
             if (PrefabUtility.GetPrefabType(streamPlayer.gameObject) == PrefabType.DisconnectedModelPrefabInstance)
+#endif
             {
                 EditorGUILayout.BeginHorizontal();
                 GUILayout.Space(16);
