4ccb7f37827028c51ff1efb202cce2b8af266734
Vlad-Andrei Lazar <vladal@unity3d.com>
2019-01-31 10:12:50 -0500

    Make AlembicTracks public so timeline can see them.

--- proto.com.unity.formats.alembic/Runtime/Scripts/Timeline/AlembicRecorderTrack.cs
+++ proto.com.unity.formats.alembic/Runtime/Scripts/Timeline/AlembicRecorderTrack.cs
@@ -4,10 +4,13 @@ using UnityEngine.Timeline;
 
 namespace UnityEngine.Formats.Alembic.Timeline
 {
+    /// <summary>
+    /// Timeline integration class for the Alembic Recorder
+    /// </summary>
     [System.Serializable]
     [TrackClipType(typeof(AlembicRecorderClip))]
     [TrackColor(0.33f, 0.0f, 0.08f)]
-    internal class AlembicRecorderTrack : TrackAsset
+    public class AlembicRecorderTrack : TrackAsset
     {
     }
 }
