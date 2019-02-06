81fbb64aa7700cee253a8b7b1316c8e185d331e6
Vlad-Andrei Lazar <vladal@unity3d.com>
2019-02-06 09:04:29 -0500

    PR comments.

--- repack.sh
+++ repack.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+#  upm-ci-utils is a PackageManager tool used to extract tests into a separate,
+#  related package.
+# To install it use:
+# npm install upm-ci-utils -g --registry https://api.bintray.com/npm/unity/unity-npm
+
+
 pushd build/install/com.unity.formats.alembic/
-#npm install upm-ci-utils -g --registry https://api.bintray.com/npm/unity/unity-npm
 upm-ci package pack
 mv automation ../
 cd ../automation/packages
