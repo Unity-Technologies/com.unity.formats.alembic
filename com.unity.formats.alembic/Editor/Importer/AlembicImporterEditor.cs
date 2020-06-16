248d403965f69442fbf84e20c3e0deacab5dc06a
Vlad-Andrei Lazar <vladal@unity3d.com>
2020-06-16 14:07:53 -0400

    FTV-759: change namespace for AlembicImporter

--- com.unity.formats.alembic/Editor/Importer/AlembicImporterEditor.cs
+++ com.unity.formats.alembic/Editor/Importer/AlembicImporterEditor.cs
@@ -5,7 +5,11 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Formats.Alembic.Sdk;
+#if UNITY_2020_2_OR_NEWER
+using UnityEditor.AssetImporters;
+#else
 using UnityEditor.Experimental.AssetImporters;
+#endif
 
 namespace UnityEditor.Formats.Alembic.Importer
 {
