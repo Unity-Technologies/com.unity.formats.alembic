184597ea9ce7692b0f888c3a54bc21c872e7aa45
Vlad-Andrei Lazar <vladal@unity3d.com>
2020-12-21 13:56:38 -0500

    Add Unit test.

--- com.unity.formats.alembic/Tests/Editor/EditorTests.cs
+++ com.unity.formats.alembic/Tests/Editor/EditorTests.cs
@@ -1,4 +1,7 @@
+using System.Linq;
 using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Formats.Alembic.Importer;
 using UnityEngine.Formats.Alembic.Sdk;
 
 namespace UnityEditor.Formats.Alembic.Exporter.UnitTests
@@ -9,6 +12,19 @@ namespace UnityEditor.Formats.Alembic.Exporter.UnitTests
         public void MarshalTests()
         {
             Assert.AreEqual(72, System.Runtime.InteropServices.Marshal.SizeOf(typeof(aePolyMeshData)));
+        }
+
+        [Test]
+        public void BadGeometryDoesNotCreateNanNormals()
+        {
+            const string dummyGUID = "04cbadf80f70d4415baf1450862310b0";
+            var path = AssetDatabase.GUIDToAssetPath(dummyGUID);
+            var abc = AssetDatabase.LoadMainAssetAtPath(path);
+            var instance = PrefabUtility.InstantiatePrefab(abc) as GameObject;
+            instance.GetComponent<AlembicStreamPlayer>().UpdateImmediately(0);
+            var mesh = instance.GetComponentInChildren<MeshFilter>().sharedMesh;
+            var naNs = mesh.normals.Where(x => double.IsNaN(x.x) || double.IsNaN(x.y) || double.IsNaN(x.z));
+            Assert.IsEmpty(naNs);
         }
     }
 }
