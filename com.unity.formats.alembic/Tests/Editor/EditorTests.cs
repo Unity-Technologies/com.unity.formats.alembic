f2f44eba9f8174d3c760eab1514556099c98ef8c
Vlad Lazar <vladal@unity3d.com>
2021-01-13 18:38:48 -0500

    ABC-184 Nan Normals (#386)
    * JIRA: ABC-184 NaN Normals
    
    Bug was caused by malformed triangles, giving rise to NaN normals.
    
    * Add Unit test.
    
    * Update unit test with lighter asset.
    
    * Add the changelog.

--- com.unity.formats.alembic/Tests/Editor/EditorTests.cs
+++ com.unity.formats.alembic/Tests/Editor/EditorTests.cs
@@ -1,4 +1,7 @@
+using System.Linq;
 using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Formats.Alembic.Importer;
 using UnityEngine.Formats.Alembic.Sdk;
 
 namespace UnityEditor.Formats.Alembic.Exporter.UnitTests
@@ -9,6 +12,19 @@ namespace UnityEditor.Formats.Alembic.Exporter.UnitTests
         public void MarshalTests()
         {
             Assert.AreEqual(72, System.Runtime.InteropServices.Marshal.SizeOf(typeof(aePolyMeshData)));
+        }
+
+        [Test]
+        public void BadGeometryDoesNotCreateNanNormals()
+        {
+            const string dummyGUID = "4f03ab724b2494f38ae7c6c3d06e0825";
+            var path = AssetDatabase.GUIDToAssetPath(dummyGUID);
+            var abc = AssetDatabase.LoadMainAssetAtPath(path);
+            var instance = PrefabUtility.InstantiatePrefab(abc) as GameObject;
+            instance.GetComponent<AlembicStreamPlayer>().UpdateImmediately(0);
+            var mesh = instance.GetComponentInChildren<MeshFilter>().sharedMesh;
+            var naNs = mesh.normals.Where(x => double.IsNaN(x.x) || double.IsNaN(x.y) || double.IsNaN(x.z));
+            Assert.IsEmpty(naNs);
         }
     }
 }
