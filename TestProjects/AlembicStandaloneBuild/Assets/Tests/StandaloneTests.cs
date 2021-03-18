7cf9e4d645f6ca1ae1ac19dc686ea3d861563578
Vlad-Andrei Lazar <vladal@unity3d.com>
2021-03-18 14:01:31 -0400

    Importless standalone build tests.

--- TestProjects/AlembicStandaloneBuild/Assets/Tests/StandaloneTests.cs
+++ TestProjects/AlembicStandaloneBuild/Assets/Tests/StandaloneTests.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.IO;
 using NUnit.Framework;
 using UnityEngine.Formats.Alembic.Importer;
 using UnityEngine.SceneManagement;
@@ -6,7 +7,7 @@ using UnityEngine.TestTools;
 
 namespace UnityEngine.Formats.Alembic.StandaloneTests
 {
-    public class StandaloneTests
+    class StandaloneTests
     {
         [SetUp]
         public void SetUp()
@@ -15,7 +16,7 @@ namespace UnityEngine.Formats.Alembic.StandaloneTests
         }
 
         [UnityTest]
-        public IEnumerator AlembicUpdatesCorrectly()
+        public IEnumerator ImportedAlembicUpdatesCorrectly()
         {
             var abc = GameObject.Find("human").GetComponent<AlembicStreamPlayer>();
             Assert.IsNotNull(abc);
@@ -27,5 +28,38 @@ namespace UnityEngine.Formats.Alembic.StandaloneTests
 
             yield return null;
         }
+
+        [UnityTest]
+        public IEnumerator ImporterlessAlembicUpdatesCorrectly()
+        {
+            var abc = GameObject.Find("Importless").GetComponent<AlembicStreamPlayer>();
+            Assert.IsNotNull(abc);
+            var mesh = abc.GetComponentInChildren<MeshFilter>().sharedMesh;
+            var position = new Vector3(0.0633399412f, 0.357630074f, 0.540497243f);
+            Assert.That((position - mesh.vertices[0]).magnitude, Is.Not.EqualTo(0).Within(1e-5));
+            abc.UpdateImmediately(1);
+            Assert.That((position - mesh.vertices[0]).magnitude, Is.EqualTo(0).Within(1e-5));
+
+            yield return null;
+        }
+
+#if UNITY_STANDALONE
+        [UnityTest]
+        public IEnumerator SpawnedAlembicUpdatesCorrectly()
+        {
+            var abc = new GameObject().AddComponent<AlembicStreamPlayer>();
+            var humanPath = Path.Combine(Path.GetTempPath(), "human.abc"); // This is being copied by the build postprocessor from the test project
+            var ret = abc.LoadFromFile(humanPath);
+            Assert.IsTrue(ret);
+            var mesh = abc.GetComponentInChildren<MeshFilter>().sharedMesh;
+            var position = new Vector3(0.0633399412f, 0.357630074f, 0.540497243f);
+            Assert.That((position - mesh.vertices[0]).magnitude, Is.Not.EqualTo(0).Within(1e-5));
+            abc.UpdateImmediately(1);
+            Assert.That((position - mesh.vertices[0]).magnitude, Is.EqualTo(0).Within(1e-5));
+
+            yield return null;
+        }
+
+#endif
     }
 }
