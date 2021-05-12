a8b6a915d4112d6abb162236d6f27a2a1d1f6c42
Vlad-Andrei Lazar <vladal@unity3d.com>
2021-05-12 13:59:19 -0400

    Velocity Unit test

--- com.unity.formats.alembic/Tests/Editor/EditorTests.cs
+++ com.unity.formats.alembic/Tests/Editor/EditorTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using NUnit.Framework;
@@ -185,6 +186,60 @@ namespace UnityEditor.Formats.Alembic.Exporter.UnitTests
             Undo.PerformUndo();
 
             Assert.IsTrue(go == null); // If the alembic importer pushed undo events, i will not undo the creation of the gameobject. If this is dead, it means there were no extra undo events between.
+        }
+
+        [Test]
+        public void Velocities_AreConsistentWithMotion()
+        {
+            var path = AssetDatabase.GUIDToAssetPath("a6d019a425afe49d7a8fd029c82c0455"); // GUID of triangleRigged_asymetricalVertexTransform_24fps_2f.abc
+            var asset = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+            var go = PrefabUtility.InstantiatePrefab(asset) as GameObject;
+            var player = go.GetComponent<AlembicStreamPlayer>();
+            var mesh = player.GetComponentInChildren<MeshFilter>().sharedMesh;
+            player.UpdateImmediately(0);
+
+            var vert0 = new List<Vector3>();
+            var velocity0 = new List<Vector3>();
+            mesh.GetVertices(vert0);
+            mesh.GetUVs(5, velocity0);
+
+            Assert.IsTrue(velocity0.All(x => x == Vector3.zero));
+
+            player.UpdateImmediately(1 / 60f);
+            var vert1 = new List<Vector3>();
+            var velocity1 = new List<Vector3>();
+            mesh.GetVertices(vert1);
+            mesh.GetUVs(5, velocity1);
+
+            var computedVelocity = vert1.Zip(vert0, (v1, v0) => v1 - v0).ToList();
+            CollectionAssert.AreEqual(velocity1, computedVelocity);
+        }
+
+        [Test]
+        public void Velocities_AreScaled()
+        {
+            var path = AssetDatabase.GUIDToAssetPath("a6d019a425afe49d7a8fd029c82c0455"); // GUID of triangleRigged_asymetricalVertexTransform_24fps_2f.abc
+            var asset = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+            var go = PrefabUtility.InstantiatePrefab(asset) as GameObject;
+            var player = go.GetComponent<AlembicStreamPlayer>();
+            var mesh = player.GetComponentInChildren<MeshFilter>().sharedMesh;
+            player.UpdateImmediately(1 / 60f);
+
+            var vert0 = new List<Vector3>();
+            var velocity0 = new List<Vector3>();
+            mesh.GetVertices(vert0);
+            mesh.GetUVs(5, velocity0);
+
+            var expectedVelocity = new[] {new Vector3(0, 8, 0), new Vector3(-4, 0, 0), new Vector3(0, 0, 0)};
+            CollectionAssert.AreEqual(expectedVelocity, velocity0);
+
+            player.VertexMotionScale = 0.5f;
+            player.UpdateImmediately(0);
+            player.UpdateImmediately(1 / 60f); // Bug that needs to change time to update velocity
+            mesh.GetUVs(5, velocity0);
+            var expected = expectedVelocity.Select(x => 0.5f * x).ToArray();
+            var error = expected.Zip(velocity0, (x, y) => (x - y).magnitude);
+            Assert.IsTrue(error.All(x => x < 1e-5));
         }
     }
 }
