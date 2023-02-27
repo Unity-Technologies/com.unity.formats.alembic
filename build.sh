0bd0e953022c4dd95d47b83b98d1e90b86409065
wind.xu <wind.xu@unity3d.com>
2023-02-27 11:41:51 -0500

    Add 'set -e' so that build script won't fail silently

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## Any subsequent(*) commands which fail will cause the shell script to exit immediately, otherwise the job will fail silently.
+set -e
+
 # Produce fast and small code (but not debuggable), and produce it to be
 # relocatable since in the end we'll link it all together in a shared object.
 # Note that cmake seems to clobber the -O3 with a -O2, but we can dream.
