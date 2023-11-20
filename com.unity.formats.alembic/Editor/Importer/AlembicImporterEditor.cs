3a498ba7324ee2158f3ed1255fc1f0f076841f56
Jun Loke <jun.loke@unity3d.com>
2023-11-20 16:13:22 -0500

    Add icon to section title

--- com.unity.formats.alembic/Editor/Importer/AlembicImporterEditor.cs
+++ com.unity.formats.alembic/Editor/Importer/AlembicImporterEditor.cs
@@ -403,7 +403,10 @@ namespace UnityEditor.Formats.Alembic.Importer
 
         void DrawHairUI(AlembicImporter importer, bool isMultiEdit)
         {
-            EditorGUILayout.LabelField("[icon] Hair", EditorStyles.boldLabel);
+            var hairLabel = EditorGUIUtility.TrTextContentWithIcon("Hair",
+                EditorGUIUtility.IconContent("info").image);
+
+            EditorGUILayout.LabelField(hairLabel, EditorStyles.boldLabel);
             {
                 GUILayout.FlexibleSpace();
                 if (GUILayout.Button("Generate Hair Assets"))
