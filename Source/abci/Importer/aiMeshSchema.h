6385bb553a683bd0e7dc9ae2703163c851efab48
SanaJal <104858438+SanaJal@users.noreply.github.com>
2023-12-07 12:18:01 -0500

    [ABC-312] Fix Face scope constant attributes (#569)
    * fix rgb working
    
    * fix for all the supported attributes
    
    * test color attribute face scope
    
    * test all scopes
    
    * refactor
    
    * commit suggestion : format
    
    * commit suggestion : make dictionary static readonly
    
    Co-authored-by: thomas-tu <45040865+thomas-tu@users.noreply.github.com>
    
    * new file for tests + remove old tests
    
    * make the tests compatible with 19.4 and 20.3
    
    * test and asset for normals
    
    * add changelog
    
    * commit suggestion : formatting
    
    Co-authored-by: thomas-tu <45040865+thomas-tu@users.noreply.github.com>
    
    * commit suggestion : update changelog
    
    Co-authored-by: Jun Loke <jun.loke@unity3d.com>
    
    ---------
    
    Co-authored-by: thomas-tu <45040865+thomas-tu@users.noreply.github.com>
    Co-authored-by: noreply@unity3d.com <noreply@unity3d.com>
    Co-authored-by: Jun Loke <jun.loke@unity3d.com>

--- Source/abci/Importer/aiMeshSchema.h
+++ Source/abci/Importer/aiMeshSchema.h
@@ -121,6 +121,8 @@ public:
 template<typename T, typename U>
 class aiMeshSchema : public aiTSchema<T>
 {
+    IArray<int>  getAttributesIndices(MeshRefiner& refiner);
+
 public:
     aiMeshSchema(aiObject* parent, const abcObject& abc);
     ~aiMeshSchema();
@@ -428,6 +430,22 @@ template<typename T, typename U>
 const aiMeshSummaryInternal& aiMeshSchema<T, U>::getSummary() const
 {
     return m_summary;
+}
+
+template<typename T, typename U>
+IArray<int> aiMeshSchema<T, U>::getAttributesIndices(MeshRefiner& refiner)
+{
+    int* indices = new int[refiner.indices.size()];
+    int m = 0;
+    for (int i = 0; i < refiner.counts.size(); i++)
+    {
+        for (int j = 0; j < refiner.counts[i]; j++)
+        {
+            indices[m] = i;
+            m++;
+        }
+    }
+    return { indices, refiner.indices.size() };
 }
 
 template<typename T, typename U>
@@ -854,6 +872,11 @@ void aiMeshSchema<T, U>::onTopologyChange(U& sample)
         {
             refiner.template addIndexedAttribute<abcV3>(src, refiner.indices, dst, topology.m_remap_normals);
         }
+        else if (src.size() == refiner.counts.size())
+        {
+            IArray<int> normals_indices = getAttributesIndices(refiner);
+            refiner.template addIndexedAttribute<abcV3>(src, normals_indices, dst, topology.m_remap_normals);
+        }
         else
         {
             DebugLog("Invalid attribute");
@@ -879,6 +902,11 @@ void aiMeshSchema<T, U>::onTopologyChange(U& sample)
         else if (src.size() == refiner.points.size())
         {
             refiner.template addIndexedAttribute<abcV2>(src, refiner.indices, dst, topology.m_remap_uv0);
+        }
+        else if (src.size() == refiner.counts.size())
+        {
+            IArray<int> uv0_indices = getAttributesIndices(refiner);
+            refiner.template addIndexedAttribute<abcV2>(src, uv0_indices, dst, topology.m_remap_uv0);
         }
         else
         {
@@ -906,6 +934,11 @@ void aiMeshSchema<T, U>::onTopologyChange(U& sample)
         {
             refiner.template addIndexedAttribute<abcV2>(src, refiner.indices, dst, topology.m_remap_uv1);
         }
+        else if (src.size() == refiner.counts.size())
+        {
+            IArray<int> uv1_indices = getAttributesIndices(refiner);
+            refiner.template addIndexedAttribute<abcV2>(src, uv1_indices, dst, topology.m_remap_uv1);
+        }
         else
         {
             DebugLog("Invalid attribute");
@@ -932,6 +965,11 @@ void aiMeshSchema<T, U>::onTopologyChange(U& sample)
         {
             refiner.template addIndexedAttribute<abcC4>(src, refiner.indices, dst, topology.m_remap_rgba);
         }
+        else if (src.size() == refiner.counts.size())
+        {
+            IArray<int> rgba_indices = getAttributesIndices(refiner);
+            refiner.template addIndexedAttribute<abcC4>(src, rgba_indices, dst, topology.m_remap_rgba);
+        }
         else
         {
             DebugLog("Invalid attribute");
@@ -957,6 +995,11 @@ void aiMeshSchema<T, U>::onTopologyChange(U& sample)
         else if (src.size() == refiner.points.size())
         {
             refiner.template addIndexedAttribute<abcC3>(src, refiner.indices, dst, topology.m_remap_rgb);
+        }
+        else if (src.size() == refiner.counts.size())
+        {
+            IArray<int> rgb_indices = getAttributesIndices(refiner);
+            refiner.template addIndexedAttribute<abcC3>(src, rgb_indices, dst, topology.m_remap_rgb);
         }
         else
         {
--- com.unity.formats.alembic/CHANGELOG.md
+++ com.unity.formats.alembic/CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [2.4.0-pre.1] - 2023-11-07
+### Fixed
+- Fixed bug where vertex color displayed as black when processing RGB, RGBA, and Normal attributes in the `Face` scope
+- Fixed bug causing Editor to crash when a UV attribute is processed in the `Points` or `Face` scope.
 
 ## [2.3.4] - 2023-10-30
 ### Fixed
