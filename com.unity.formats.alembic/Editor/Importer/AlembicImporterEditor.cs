b9f5eceae525bdb93c2c8c40681a70a8bd3f3765
Jun Loke <jun.loke@unity3d.com>
2024-01-22 13:51:59 -0500

    [ABC-475] add padding to help box (#577)
    * add padding to help box
    
    * Fix styling issue with HelpBox. Also made our own helper so the control is available to other part of the UI
    
    * Fix indent issue
    
    * Fix double localization
    
    * Add missing xmldoc
    
    ---------
    
    Co-authored-by: Thomas Tu <thomastu@unity3d.com>

--- com.unity.formats.alembic/Editor/Importer/AlembicImporterEditor.cs
+++ com.unity.formats.alembic/Editor/Importer/AlembicImporterEditor.cs
@@ -57,16 +57,16 @@ namespace UnityEditor.Formats.Alembic.Importer
             {
                 GUILayout.FlexibleSpace();
                 uiTab.value = GUILayout.Toolbar(uiTab,
-                    new[] {L10n.Tr("Model"), L10n.Tr("Materials"), L10n.Tr("Hair")});
+                    new[] { L10n.Tr("Model"), L10n.Tr("Materials"), L10n.Tr("Hair") });
                 GUILayout.FlexibleSpace();
             }
 
             switch (uiTab)
             {
-                case (int) UITab.Material:
+                case (int)UITab.Material:
                     DrawMaterialUI(importer, serializedObject.isEditingMultipleObjects);
                     break;
-                case (int) UITab.Hair:
+                case (int)UITab.Hair:
                     DrawHairUI(importer);
                     break;
                 default:
@@ -210,7 +210,10 @@ namespace UnityEditor.Formats.Alembic.Importer
                 var toks = r.Key.name.Split(':');
                 var entry = new AlembicImporter.MaterialEntry
                 {
-                    path = toks[0], index = int.Parse(toks[1]), facesetName = toks[2], material = r.Value as Material
+                    path = toks[0],
+                    index = int.Parse(toks[1]),
+                    facesetName = toks[2],
+                    material = r.Value as Material
                 };
                 ret.Add(entry);
             }
@@ -414,132 +417,73 @@ namespace UnityEditor.Formats.Alembic.Importer
             // var hairLabel = EditorGUIUtility.TrTextContentWithIcon("Hair",
             //     EditorGUIUtility.IconContent("info").image); // Title with Icon
 
-            EditorGUILayout.LabelField(hairLabel, EditorStyles.boldLabel);
+            using (new EditorGUILayout.VerticalScope())
             {
-                GUILayout.FlexibleSpace();
+                EditorGUILayout.LabelField(hairLabel, EditorStyles.boldLabel);
+                {
+                    GUILayout.FlexibleSpace();
 
 #if !HAIR_AVAILABLE
-                string msg = L10n.Tr("Hair package not found. " +
-                                     "You have to install it first to generate " +
-                                     "a curve-based groom.");
+                    string msg = "Hair package not found. " +
+                                 "You have to install it first to generate " +
+                                 "a curve-based groom.";
 
-                GUILayout.FlexibleSpace();
-                ButtonHelpbox(msg, L10n.Tr("Install"),
-                () =>
-                {
-                    const string hairRepoLink = "https://github.com/Unity-Technologies/com.unity.demoteam.hair";
-                    Application.OpenURL(hairRepoLink);
-
-                    // Use this when Hair package is published
-                    // Client.Add("com.unity.demoteam.hair");
-                });
-#else
-                var go = AssetDatabase.LoadMainAssetAtPath(importer.assetPath) as GameObject;
-                var alembicStreamPlayer = go.GetComponent<AlembicStreamPlayer>();
-
-                if (HasCurves(alembicStreamPlayer))
-                {
-                    if (GUILayout.Button(L10n.Tr("Generate Hair Asset")))
+                    GUILayout.FlexibleSpace();
+                    UIHelper.HelpBoxWithAction(msg, MessageType.Warning, L10n.Tr("Install"),
+                    () =>
                     {
-                        string path = Path.GetDirectoryName(importer.assetPath) + "/" + go.name + "_Hair.asset";
-                        path = AssetDatabase.GenerateUniqueAssetPath(path);
+                        const string hairRepoLink = "https://github.com/Unity-Technologies/com.unity.demoteam.hair";
+                        Application.OpenURL(hairRepoLink);
 
-                        var hairAsset = CreateInstance<HairAsset>();
-                        hairAsset.name = go.name + "_Hair";
-                        hairAsset.settingsBasic.type = HairAsset.Type.Alembic;
-                        hairAsset.settingsAlembic.alembicAsset = alembicStreamPlayer;
-                        AssetDatabase.CreateAsset(hairAsset, path);
+                        // Use this when Hair package is published
+                        // Client.Add("com.unity.demoteam.hair");
+                    });
+#else
+                    var go = AssetDatabase.LoadMainAssetAtPath(importer.assetPath) as GameObject;
+                    var alembicStreamPlayer = go.GetComponent<AlembicStreamPlayer>();
 
-                        HairAssetBuilder.BuildHairAsset(hairAsset);
-                        EditorGUIUtility.PingObject(hairAsset);
-                        AssetDatabase.SaveAssetIfDirty(hairAsset);
-                        Selection.activeObject = hairAsset;
+                    if (HasCurves(alembicStreamPlayer))
+                    {
+                        if (GUILayout.Button(L10n.Tr("Generate Hair Asset")))
+                        {
+                            string path = Path.GetDirectoryName(importer.assetPath) + "/" + go.name + "_Hair.asset";
+                            path = AssetDatabase.GenerateUniqueAssetPath(path);
+
+                            var hairAsset = CreateInstance<HairAsset>();
+                            hairAsset.name = go.name + "_Hair";
+                            hairAsset.settingsBasic.type = HairAsset.Type.Alembic;
+                            hairAsset.settingsAlembic.alembicAsset = alembicStreamPlayer;
+                            AssetDatabase.CreateAsset(hairAsset, path);
+
+                            HairAssetBuilder.BuildHairAsset(hairAsset);
+                            EditorGUIUtility.PingObject(hairAsset);
+                            AssetDatabase.SaveAssetIfDirty(hairAsset);
+                            Selection.activeObject = hairAsset;
+                        }
+
+                        GUI.enabled = true;
                     }
+                    else
+                    {
+                        GUI.enabled = false;
+                        GUILayout.Button(L10n.Tr("Generate Hair Asset"));
+                        GUI.enabled = true;
 
-                    GUI.enabled = true;
-                }
-                else
-                {
-                    GUI.enabled = false;
-                    GUILayout.Button(L10n.Tr("Generate Hair Asset"));
-                    GUI.enabled = true;
-
-                    var message = L10n.Tr("Unable to locate curves in the Alembic asset. " +
-                                          "Ensure that the asset contains curves and \"Import Curves\" is " +
-                                          "enabled in the \"Model\" tab.");
-                    EditorGUILayout.HelpBox(message, MessageType.Warning);
-                }
+                        var message = "Unable to locate curves in the Alembic asset. " +
+                                      "Ensure that the asset contains curves and \"Import Curves\" is " +
+                                      "enabled in the \"Model\" tab.";
+                        EditorGUILayout.HelpBox(message, MessageType.Warning);
+                    }
 #endif
+                }
             }
         }
-
 
         bool HasCurves(AlembicStreamPlayer alembicStreamPlayer)
         {
             return (alembicStreamPlayer != null
                     && alembicStreamPlayer.GetComponentInChildren<AlembicCurves>(includeInactive: true) != null);
         }
-
-        const float k_IndentMargin = 15.0f;
-
-        /// <summary>Draw a help box with a button.</summary>
-        /// <param name="message">The message.</param>
-        /// <param name="buttonLabel">The button text.</param>
-        /// <param name="action">When the user clicks the button, Unity performs this action.</param>
-        void ButtonHelpbox(string message, string buttonLabel, Action action)
-        {
-            // code taken from UnityCoreUtils.FixMeBox()
-            var content = EditorGUIUtility.TrTextContentWithIcon(
-                message, EditorGUIUtility.TrIconContent("console.warnicon").image);
-            EditorGUILayout.BeginHorizontal();
-
-            float indent = EditorGUI.indentLevel * k_IndentMargin - EditorStyles.helpBox.margin.left;
-            GUILayoutUtility.GetRect(
-                indent, EditorGUIUtility.singleLineHeight,
-                EditorStyles.helpBox, GUILayout.ExpandWidth(false));
-
-            Rect leftRect = GUILayoutUtility.GetRect(
-                new GUIContent(buttonLabel), EditorStyles.miniButton, GUILayout.MinWidth(60));
-            Rect rect = GUILayoutUtility.GetRect(content, EditorStyles.helpBox);
-            Rect boxRect = new Rect(leftRect.x, rect.y, rect.xMax - leftRect.xMin, rect.height);
-
-            int oldIndent = EditorGUI.indentLevel;
-            EditorGUI.indentLevel = 0;
-
-            if (Event.current.type == EventType.Repaint)
-                EditorStyles.helpBox.Draw(boxRect, false, false, false, false);
-
-            Rect labelRect = new Rect(boxRect.x + 4, boxRect.y + 3, rect.width - 8, rect.height);
-            EditorGUI.LabelField(labelRect, content, HelpBox());
-
-            var buttonRect = leftRect;
-            buttonRect.x += rect.width - 2;
-            buttonRect.y = rect.yMin + (rect.height - EditorGUIUtility.singleLineHeight) / 2;
-            bool clicked = GUI.Button(buttonRect, buttonLabel);
-
-            EditorGUI.indentLevel = oldIndent;
-            GUILayout.FlexibleSpace();
-
-            EditorGUILayout.EndHorizontal();
-
-            if (clicked)
-                action();
-
-        }
-        static GUIStyle HelpBox()
-        {
-            var style = new GUIStyle()
-            {
-                imagePosition = ImagePosition.ImageLeft,
-                fontSize = 10,
-                wordWrap = true,
-                alignment = TextAnchor.MiddleLeft
-            };
-            style.normal.textColor = EditorStyles.helpBox.normal.textColor;
-            return style;
-
-        }
-
     }
 
     class SavedInt
---
+++ com.unity.formats.alembic/Editor/Misc/UIHelper.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using System;
+
+namespace UnityEditor.Formats.Alembic
+{
+    class UIHelper
+    {
+        const float k_IndentMargin = 15.0f;
+
+        static GUIStyle s_HelpBoxStyle = null;
+
+        static GUIStyle helpBoxStyle
+        {
+            get
+            {
+                if (s_HelpBoxStyle == null)
+                {
+                    s_HelpBoxStyle = new GUIStyle()
+                    {
+                        imagePosition = ImagePosition.ImageLeft,
+                        fontSize = 10,
+                        wordWrap = true,
+                        alignment = TextAnchor.MiddleLeft
+                    };
+                    s_HelpBoxStyle.normal.textColor = EditorStyles.helpBox.normal.textColor;
+                }
+                return s_HelpBoxStyle;
+            }
+        }
+
+        /// <summary>Draw a help box with a button.</summary>
+        /// <param name="message">The message, unlocalized.</param>
+        /// <param name="messageType">How the message box should appear. This affects the icon shown in the UI.</param>
+        /// <param name="buttonLabel">The button text.</param>
+        /// <param name="action">When the user clicks the button, Unity performs this action.</param>
+        internal static void HelpBoxWithAction(string message, MessageType messageType, string buttonLabel, Action action)
+        {
+            var messageContent = EditorGUIUtility.TrTextContentWithIcon(message, messageType);
+
+            EditorGUILayout.BeginHorizontal();
+
+            float indent = EditorGUI.indentLevel * k_IndentMargin - EditorStyles.helpBox.margin.left;
+            GUILayoutUtility.GetRect(indent, EditorGUIUtility.singleLineHeight, EditorStyles.helpBox, GUILayout.ExpandWidth(false));
+
+            Rect leftRect = GUILayoutUtility.GetRect(new GUIContent(buttonLabel), EditorStyles.miniButton, GUILayout.MinWidth(60), GUILayout.ExpandWidth(false));
+            Rect rect = GUILayoutUtility.GetRect(messageContent, EditorStyles.helpBox);
+            Rect boxRect = new Rect(leftRect.x, rect.y, rect.xMax - leftRect.xMin, rect.height);
+
+            int oldIndent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            if (Event.current.type == EventType.Repaint)
+                EditorStyles.helpBox.Draw(boxRect, false, false, false, false);
+
+            Rect labelRect = new Rect(boxRect.x + 4, boxRect.y, rect.width - 8, rect.height);
+            EditorGUI.LabelField(labelRect, messageContent, helpBoxStyle);
+
+            var buttonRect = leftRect;
+            buttonRect.x += rect.width - 2;
+            buttonRect.y = rect.yMin + (rect.height - EditorGUIUtility.singleLineHeight) / 2;
+            bool clicked = GUI.Button(buttonRect, buttonLabel);
+
+            EditorGUI.indentLevel = oldIndent;
+            EditorGUILayout.EndHorizontal();
+
+            if (clicked)
+                action();
+        }
+    }
+}
