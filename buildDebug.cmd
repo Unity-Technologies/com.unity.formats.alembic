281fc433e214cee7f84681abf0f15b12aa98bc21
windxu88 <45211214+windxu88@users.noreply.github.com>
2024-05-13 15:31:02 +0000

    Add build job for ARM64 Windows (#599)
    * Add a build job for ARM64 Win and remove the pre-built abci.dll
    
    * Stop using CMAKE_SYSTEM_PROCESSOR on ARM64 Win VM because of known issue
    
    * Fix set command and add error check
    
    * Add a debug line
    
    * Replace last CMAKE_SYSTEM_PROCESSOR
    
    * Also update buildDebug.cmd and remove 2023.2 from CI

--- buildDebug.cmd
+++ buildDebug.cmd
@@ -2,10 +2,20 @@
 
 cd External
 call buildDebug.cmd
+
+if %ERRORLEVEL% NEQ 0 (
+	echo Failed to build ilmbase lib or alembic lib
+	exit 1
+)
 cd ..
 
 SET depsdir=%cd%\External\install
 SET installdir=%cd%
+if "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
+    SET targetArch=ARM64
+) else (
+    SET targetArch=x64
+)
 
 
 if exist build (
@@ -13,7 +23,10 @@ if exist build (
 )
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug ^
+cmake .. ^
+    -A %targetArch% ^
+    -DCMAKE_GENERATOR_PLATFORM=%targetArch% ^ 
+    -DCMAKE_BUILD_TYPE=Debug ^
     -DALEMBIC_DIR=%depsdir% ^
     -DUSE_STATIC=ON ^
     -DENABLE_DEPLOY=OFF ^
@@ -21,4 +34,9 @@ cmake .. -DCMAKE_BUILD_TYPE=Debug ^
     -DCMAKE_INSTALL_PREFIX=%installdir% ^
     -DCMAKE_CXX_FLAGS="/MP"
 cmake --build . --target INSTALL --config Debug
+
+if %ERRORLEVEL% NEQ 0 (
+	echo Failed to build ilmbase or alembic
+	exit 1
+)
 cd ..
