8bdd0f44364f916cf8c69e8fe7ce1c7e943f5aaa
wind.xu <wind.xu@unity3d.com>
2025-02-12 15:32:41 -0500

    Add Wrench project to Alembic CI

---
+++ Tools/CI/Recipes/BuildAlembicPlugins.cs
@@ -0,0 +1,80 @@
+using Alembic.Cookbook.Settings;
+using DotLiquid.Util;
+using RecipeEngine.Api.Dependencies;
+using RecipeEngine.Api.Extensions;
+using RecipeEngine.Api.Jobs;
+using RecipeEngine.Api.Platforms;
+using RecipeEngine.Api.Recipes;
+using RecipeEngine.Platforms;
+
+namespace Alembic.Cookbook.Recipes;
+
+public class BuildAlembicPlugins : RecipeBase
+{
+    protected override ISet<Job> LoadJobs()
+        => Combine.Collections(GetJobs()).SelectJobs();
+
+    public string GetJobName(Agent agent)
+        => $"Build plugins - {agent.Image.Split(new []{'/', ':'})[1]}";
+
+    public IEnumerable<Dependency> AsDependencies()
+        => this.Jobs.ToDependencies(this);
+
+    public IEnumerable<IJobBuilder> GetJobs()
+    {
+        var settings = AlembicSettings.Instance;
+
+        var platforms = settings.Wrench.Packages[AlembicSettings.AlembicPackageName].EditorPlatforms;
+        //Agents to build plugins on which include Win, WinArm64, Mac and Ubuntu
+        List<Agent> buildAgents = new();
+        foreach (var platform in platforms)
+        {
+            buildAgents.Add(platform.Value.Agent);
+        }
+
+        //Add WinArm64 to build agents
+        Agent winArm64 = new Agent("package-ci/win11-arm64:default", FlavorType.BuildExtraLarge, ResourceType.Azure, "arm");
+        buildAgents.Add(winArm64);
+
+
+        List<IJobBuilder> builders = new ();
+        foreach (var agent in buildAgents)
+        {
+            var builder = JobBuilder.Create(GetJobName(agent))
+                .WithPlatform(new Platform(agent, SystemType.Unknown))
+                .WithDescription(GetJobName(agent));
+
+            if (agent.Image.Contains("win"))
+            {
+                builder.WithCommands(c => c
+                    .Add("git submodule update --init --recursive")
+                    .Add("build.cmd"));
+                if (agent.Image.Contains("arm64"))
+                {
+                    builder.WithArtifact("plugins", "com.unity.formats.alembic/Runtime/Plugins/ARM64/abc*.dll");
+                }
+                else
+                {
+                    builder.WithArtifact("plugins", "com.unity.formats.alembic/Runtime/Plugins/x86_64/abc*.dll");
+                }
+            }
+            else if (agent.Image.Contains("mac"))
+            {
+                builder.WithCommands(c => c
+                    .Add("git submodule update --init --recursive")
+                    .Add("./build.sh"))
+                    .WithArtifact("plugins", "com.unity.formats.alembic/Runtime/Plugins/x86_64/abci.bundle/Contents/MacOS/abc*");
+            }
+            else
+            {
+                builder.WithCommands(c => c
+                        .Add("git submodule update --init --recursive")
+                        .Add("./build.sh"))
+                    .WithArtifact("plugins", "com.unity.formats.alembic/Runtime/Plugins/x86_64/abc*.so");
+            }
+            builders.Add(builder);
+        }
+
+        return builders;
+    }
+}
---
+++ Tools/CI/Settings/AlembicSettings.cs
@@ -0,0 +1,63 @@
+﻿using RecipeEngine.Api.Dependencies;
+using RecipeEngine.Api.Settings;
+using RecipeEngine.Modules.Wrench.Models;
+using RecipeEngine.Modules.Wrench.Settings;
+
+namespace Alembic.Cookbook.Settings;
+
+public class AlembicSettings : AnnotatedSettingsBase
+{
+    // Path from the root of the repository where packages are located.
+    readonly string[] PackagesRootPaths = {"./com.unity.formats.alembic"};
+
+    public static readonly string AlembicPackageName = "com.unity.formats.alembic";
+
+    //A singleton instance of AlembicSettings
+    static AlembicSettings _instance;
+
+    // update this to list all packages in this repo that you want to release.
+    Dictionary<string, PackageOptions> PackageOptions = new()
+    {
+        {
+            AlembicPackageName,
+            new PackageOptions()
+            {
+                ReleaseOptions = new ReleaseOptions() { IsReleasing = true },
+                PackJobOptions = new PackJobOptions()
+                {
+                    Dependencies = new List<Dependency>()
+                    {
+                        new Dependency("", "")
+                    }
+                }
+            }
+        }
+    };
+
+    public AlembicSettings()
+    {
+        Wrench = new WrenchSettings(
+            PackagesRootPaths,
+            PackageOptions
+        );
+
+        Wrench.BranchNamingPattern = BranchPatterns.ReleaseSlash;
+        // Add "rme", "supported" to upm-pvp check profiles together with an exemption file
+        // PVP-41-1 check is skipped because of "Unreleased" entry in CHANGELOG.md
+        Wrench.PvpProfilesToCheck = new HashSet<string>() { "rme", "supported", "-PVP-41-1", ".yamato/wrench/pvp-exemptions.json" };
+    }
+
+    public WrenchSettings Wrench { get; private set; }
+
+    public static AlembicSettings Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new AlembicSettings();
+            }
+            return _instance;
+        }
+    }
+}
