3d486c22f22d65278f910f0835128afdb8f2a36e
windxu88 <45211214+windxu88@users.noreply.github.com>
2025-03-14 08:58:34 -0400

    Migrate to Wrench/PackageWorks release process (#613)
    * Add Wrench project to Alembic CI
    
    * Add plugin build jobs as dependencies of Pack job
    
    * Try disable batch mode
    
    * Change the way of defining dict in python to avoid yml parsing error
    
    * Fix python command for Win
    
    * Fix command
    
    * Use the same images for build jobs as before
    
    * Regenerate CI
    
    * Fix win image
    
    * Apply workaround to all validation jobs
    
    * Remove Info.plist.meta
    
    * Upgrade Wrench to 0.10.43
    
    * Fix format job
    
    * Apply formatting changes
    
    * Update Wrench to 0.10.44 and apply workaround for batch mode
    
    * Clean up pvp exemptions

---
+++ Tools/CI/Recipes/BuildAlembicPlugins.cs
@@ -0,0 +1,79 @@
+using Alembic.Cookbook.Settings;
+using RecipeEngine.Api.Dependencies;
+using RecipeEngine.Api.Extensions;
+using RecipeEngine.Api.Jobs;
+using RecipeEngine.Api.Platforms;
+using RecipeEngine.Api.Recipes;
+using RecipeEngine.Platforms;
+
+namespace Alembic.Cookbook.Recipes;
+
+public class BuildAlembicPlugins : RecipeBase
+{
+    protected override ISet<Job> LoadJobs()
+        => Combine.Collections(GetJobs()).SelectJobs();
+
+    public string GetJobName(Agent agent)
+        => $"Build plugins - {agent.Image.Split(new[] { '/', ':' })[1]}";
+
+    public IEnumerable<Dependency> AsDependencies()
+        => this.Jobs.ToDependencies(this);
+
+    public IEnumerable<IJobBuilder> GetJobs()
+    {
+        var settings = AlembicSettings.Instance;
+
+        List<Agent> buildAgents = new();
+
+        //Build agents
+        Agent winArm64 = new Agent("package-ci/win11-arm64:default", FlavorType.BuildLarge, ResourceType.Azure, "arm");
+        Agent win = new Agent("package-ci/win10:default", FlavorType.BuildLarge, ResourceType.Vm);
+        Agent centOS = new Agent("package-ci/centos:latest", FlavorType.BuildLarge, ResourceType.Vm);
+        Agent mac = new Agent("package-ci/macos-12:default", FlavorType.MacDefault, ResourceType.VmOsx);
+
+        buildAgents.Add(winArm64);
+        buildAgents.Add(win);
+        buildAgents.Add(centOS);
+        buildAgents.Add(mac);
+
+        List<IJobBuilder> builders = new();
+        foreach (var agent in buildAgents)
+        {
+            var builder = JobBuilder.Create(GetJobName(agent))
+                .WithPlatform(new Platform(agent, SystemType.Unknown))
+                .WithDescription(GetJobName(agent));
+
+            if (agent.Image.Contains("win"))
+            {
+                builder.WithCommands(c => c
+                    .Add("git submodule update --init --recursive")
+                    .Add("build.cmd"));
+                if (agent.Image.Contains("arm64"))
+                {
+                    builder.WithArtifact("plugins", "com.unity.formats.alembic/Runtime/Plugins/ARM64/abc*.dll");
+                }
+                else
+                {
+                    builder.WithArtifact("plugins", "com.unity.formats.alembic/Runtime/Plugins/x86_64/abc*.dll");
+                }
+            }
+            else if (agent.Image.Contains("mac"))
+            {
+                builder.WithCommands(c => c
+                    .Add("git submodule update --init --recursive")
+                    .Add("./build.sh"))
+                    .WithArtifact("plugins", "com.unity.formats.alembic/Runtime/Plugins/x86_64/abci.bundle/Contents/MacOS/abc*");
+            }
+            else
+            {
+                builder.WithCommands(c => c
+                        .Add("git submodule update --init --recursive")
+                        .Add("scl enable devtoolset-7 ./build.sh"))
+                    .WithArtifact("plugins", "com.unity.formats.alembic/Runtime/Plugins/x86_64/abc*.so");
+            }
+            builders.Add(builder);
+        }
+
+        return builders;
+    }
+}
