c6326d9cb5e2df175d6a9d306949eabc02027280
leonardo <leonardo@unity3d.com>
2026-01-27 18:23:14 +0100

    Moving the editmode test to the HDRLatest project, since the previous was only used for playmode tests

--- TestProjects/AlembicHDRPLatest/Assets/Tests/EditorModeTests.cs
+++ TestProjects/AlembicHDRPLatest/Assets/Tests/EditorModeTests.cs
@@ -13,5 +13,27 @@ namespace UnityEditor.Formats.Alembic.Tests
             Assert.IsTrue(true);
             yield return new ExitPlayMode();
         }
+
+        [Test]
+        public void CorruptAlembicFileDoesNotCrashInEditor()
+        {
+            var fileSource = "Assets/Data~/CrashAsset.abc";
+            //generate unique name, as previous import failures might prevent the import process to run.
+            var fileDestination = $"Assets/CrashAsset-{UnityEngine.GUID.Generate().ToString()}.abc";
+
+            try
+            {
+                AssetDatabase.StartAssetEditing();
+                FileUtil.CopyFileOrDirectory(fileSource, fileDestination);
+                AssetDatabase.StopAssetEditing();
+                AssetDatabase.ImportAsset(fileDestination);
+            }
+            finally
+            {
+                AssetDatabase.DeleteAsset(fileDestination);
+            }
+
+            Assert.Pass("Editor has not crashed.");
+        }
     }
 }
