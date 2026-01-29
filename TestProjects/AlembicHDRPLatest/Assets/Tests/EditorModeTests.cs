3031ca3ded55fdd61b93d5688e39e0d4aa2ddff3
Leonardo Carneiro <leonardo@unity3d.com>
2026-01-29 13:55:31 +0100

    Catching the exception before continuing back to managed. (#623)
    * Catching the exception before continuing back to managed.
    
    * fix formatting
    
    * Committing formatting fixes for existing files
    
    * Adding automated test
    
    * Fix formatting
    
    * Moving the editmode test to the HDRLatest project, since the previous was only used for playmode tests
    
    * fixing the test on older versions of Unity where GUID is in another namespace
    
    * Updating changelog

--- TestProjects/AlembicHDRPLatest/Assets/Tests/EditorModeTests.cs
+++ TestProjects/AlembicHDRPLatest/Assets/Tests/EditorModeTests.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
 using UnityEngine.TestTools;
 
 namespace UnityEditor.Formats.Alembic.Tests
@@ -12,6 +14,28 @@ namespace UnityEditor.Formats.Alembic.Tests
             yield return new EnterPlayMode();
             Assert.IsTrue(true);
             yield return new ExitPlayMode();
+        }
+
+        [Test]
+        public void CorruptAlembicFileDoesNotCrashInEditor()
+        {
+            var fileSource = "Assets/Data~/CrashAsset.abc";
+            //generate unique name, as previous import failures might prevent the import process to run.
+            var fileDestination = $"Assets/CrashAsset-{GUID.Generate().ToString()}.abc";
+
+            try
+            {
+                AssetDatabase.StartAssetEditing();
+                FileUtil.CopyFileOrDirectory(fileSource, fileDestination);
+                AssetDatabase.StopAssetEditing();
+                AssetDatabase.ImportAsset(fileDestination);
+            }
+            finally
+            {
+                AssetDatabase.DeleteAsset(fileDestination);
+            }
+
+            Assert.Pass("Editor has not crashed.");
         }
     }
 }
